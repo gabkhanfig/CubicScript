42e99742c7ffc774b71ac404d5d9b7b5d3868753
gabkhanfig <gabkhanfig@gmail.com>
2024-03-12 18:12:36 -0400

    Implement Atomic Ref Counting

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const testing = std.testing;
 
+pub const primitives = @import("types/primitives.zig");
+
 pub fn cubicScriptTest() void {
     std.debug.print("cubic script!!!\n", .{});
 }
