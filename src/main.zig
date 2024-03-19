ad67df12eaa6fd9a9e066cb8ca71f8055a1473d4
gabkhanfig <gabkhanfig@gmail.com>
2024-03-19 01:43:56 -0400

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,8 @@
 const std = @import("std");
 const cubic_script = @import("cubic_script");
-const String = cubic_script.primitives.String;
+const String = cubic_script.String;
+const ValueTag = cubic_script.ValueTag;
+const Map = cubic_script.Map;
+const TaggedValue = cubic_script.TaggedValue;
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    var s = try String.initSlice("hello to this glorious world!", allocator);
-    defer s.deinit(allocator);
-
-    std.debug.assert(s.eqlSlice("hello to this glorious world!"));
-}
+pub fn main() !void {}
