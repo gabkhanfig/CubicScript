3a4e7517d10f86c43e6214e26703c720be8df5d6
gabkhanfig <gabkhanfig@gmail.com>
2024-03-22 23:32:08 -0400

    Use Same Stack Size As MSVC

--- src/state/Stack.zig
+++ src/state/Stack.zig
@@ -10,11 +10,13 @@ const CubicScriptState = @import("CubicScriptState.zig");
 const RawValue = root.RawValue;
 
 // https://the-ravi-programming-language.readthedocs.io/en/latest/lua_bytecode_reference.html
+// https://learn.microsoft.com/en-us/cpp/build/reference/stack-stack-allocations?view=msvc-170
 
 /// Up to 256 different `RawValue`'s can be stored per stack frame.
 pub const MAX_STACK_FRAME_SIZE = 256;
-const STACK_SPACES: comptime_int = 1024 * 1024 * 2;
-/// About 16 Megabytes
+const STACK_SPACES: comptime_int = 1024 * 128;
+/// 1 Megabyte. Uses the same stack size as the MSVC default.
+/// https://learn.microsoft.com/en-us/cpp/build/reference/stack-stack-allocations?view=msvc-170
 pub const STACK_SIZE: comptime_int = @sizeOf(RawValue) * STACK_SPACES;
 
 const Self = @This();
