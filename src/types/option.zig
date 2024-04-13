8a0c427d0bf97bb535e0a6af6ec4d1cec28996af
gabkhanfig <gabkhanfig@gmail.com>
2024-04-13 00:23:13 -0400

    Implement C Compatible Primitive Option Type

--- src/types/option.zig
+++ src/types/option.zig
@@ -4,73 +4,94 @@ const assert = std.debug.assert;
 const expect = std.testing.expect;
 const root = @import("../root.zig");
 const TaggedValue = root.TaggedValue;
-const CubicScriptState = @import("../state/CubicScriptState.zig");
+const RawValue = root.RawValue;
+const ValueTag = root.ValueTag;
 const allocator = @import("../state/global_allocator.zig").allocator;
 
+/// Can be zero initialized, meaning it's a none value.
 pub const Option = extern struct {
+    const PTR_BITMASK: usize = 0xFFFFFFFFFFFF;
+    const TAG_BITMASK: usize = ~PTR_BITMASK;
+    const SHIFT = 48;
+
     const Self = @This();
 
-    /// Has ownership of the tagged value, allocated somewhere.
-    value: ?*TaggedValue,
+    inner: usize = 0,
 
-    pub fn init(inValue: ?TaggedValue) Self {
-        if (inValue == null) {
-            return Self{ .value = null };
+    pub fn init(inValue: TaggedValue) Self {
+        if (inValue.tag == .None) {
+            return Self{};
         }
-        const value = allocator().create(TaggedValue) catch {
+        const value = allocator().create(RawValue) catch {
             @panic("Script out of memory");
         };
-        value.* = inValue.?;
-        return Self{ .value = value };
+        value.* = inValue.value;
+        return Self{ .inner = @shlExact(inValue.tag.asUsize(), SHIFT) | @intFromPtr(value) };
     }
 
     pub fn deinit(self: *Self) void {
-        if (self.value == null) {
+        if (self.isNone()) {
             return;
-        }
-
-        self.value.?.deinit();
-        allocator().destroy(self.value.?);
-        self.value = null;
-    }
-
-    pub fn take(self: *Self) Error!TaggedValue {
-        if (self.value) |value| {
-            const temp = value.*; // take ownership
-            allocator().destroy(value);
-            self.value = null;
-            return temp;
         } else {
-            return Error.IsNull;
+            var taken = self.take();
+            taken.deinit();
         }
     }
 
-    pub const Error = error{
-        IsNull,
-    };
+    pub fn isNone(self: *const Self) bool {
+        return self.inner == 0;
+    }
+
+    pub fn tag(self: *const Self) ValueTag {
+        return @enumFromInt(@shrExact(self.inner & TAG_BITMASK, SHIFT));
+    }
+
+    pub fn get(self: *const Self) *const RawValue {
+        assert(!self.isNone());
+        return @ptrCast(@alignCast(@as(*const anyopaque, @ptrFromInt(self.inner & PTR_BITMASK))));
+    }
+
+    pub fn getMut(self: *Self) *RawValue {
+        assert(!self.isNone());
+        return @ptrCast(@alignCast(@as(*anyopaque, @ptrFromInt(self.inner & PTR_BITMASK))));
+    }
+
+    pub fn take(self: *Self) TaggedValue {
+        assert(!self.isNone());
+
+        const valuePtr = self.getMut();
+        const rawValue = valuePtr.*; // take ownership
+        const valueTag = self.tag();
+
+        allocator().destroy(valuePtr);
+
+        self.inner = 0;
+        return .{ .tag = valueTag, .value = rawValue };
+    }
 };
 
 test "null" {
-    var opt = Option{ .value = null };
+    var opt = Option{};
     defer opt.deinit();
 
-    if (opt.take()) |_| {
-        try expect(false);
-    } else |_| {}
+    try expect(opt.isNone());
 }
 
 test "not null" {
     var opt = Option.init(TaggedValue.initString(root.String.initSlice("aa")));
     defer opt.deinit();
 
-    try expect(opt.value != null);
+    try expect(opt.isNone() == false);
+    try expect(opt.get().string.eqlSlice("aa"));
+}
 
-    if (opt.take()) |string| {
-        try expect(string.tag == root.ValueTag.String);
-        try expect(string.value.string.eqlSlice("aa"));
-        var s = string;
-        s.value.string.deinit();
-    } else |_| {
-        try expect(false);
-    }
+test "take" {
+    var opt = Option.init(TaggedValue.initString(root.String.initSlice("aa")));
+    defer opt.deinit();
+
+    var take = opt.take();
+    defer take.deinit();
+
+    try expect(take.tag == .String);
+    try expect(take.value.string.eqlSlice("aa"));
 }
