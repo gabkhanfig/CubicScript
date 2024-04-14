e465f7d13ad27986dcb118104984bd2ef94c1f6e
gabkhanfig <gabkhanfig@gmail.com>
2024-04-14 17:31:15 -0400

    Reserve 0 As "Ok" Value For Extern C

--- src/state/Errors.zig
+++ src/state/Errors.zig
@@ -1,16 +1,16 @@
 pub const RuntimeError = enum(c_int) {
-    NullDereference = 0,
-    AdditionIntegerOverflow = 1,
-    SubtractionIntegerOverflow = 2,
-    MultiplicationIntegerOverflow = 3,
-    DivisionIntegerOverflow = 4,
-    DivideByZero = 5,
-    ModuloByZero = 6,
-    RemainderByZero = 7,
-    PowerIntegerOverflow = 8,
-    ZeroToPowerOfNegative = 9,
-    InvalidBitShiftAmount = 10,
-    FloatToIntOverflow = 11,
+    NullDereference = 1,
+    AdditionIntegerOverflow = 2,
+    SubtractionIntegerOverflow = 3,
+    MultiplicationIntegerOverflow = 4,
+    DivisionIntegerOverflow = 5,
+    DivideByZero = 6,
+    ModuloByZero = 7,
+    RemainderByZero = 8,
+    PowerIntegerOverflow = 9,
+    ZeroToPowerOfNegative = 10,
+    InvalidBitShiftAmount = 11,
+    FloatToIntOverflow = 12,
 };
 
 pub const Severity = enum(c_int) {
