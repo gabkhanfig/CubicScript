a1952123a8e0e170add54699b589dc541860945d
gabkhanfig <gabkhanfig@gmail.com>
2024-04-26 03:22:25 -0400

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,4 @@
 const std = @import("std");
 const cubic_script = @import("cubic_script");
-const String = cubic_script.String;
-const ValueTag = cubic_script.ValueTag;
-const Map = cubic_script.Map;
-const TaggedValue = cubic_script.TaggedValue;
-const CubicScriptState = cubic_script.CubicScriptState;
-const Stack = cubic_script.Stack;
-const Bytecode = cubic_script.Bytecode;
-const OpCode = Bytecode.OpCode;
 
-pub fn main() !void {
-    const mem = cubic_script.allocator().alloc(u8, 2) catch unreachable;
-    std.debug.print("{}\n", .{mem[0]});
-}
+pub fn main() !void {}
