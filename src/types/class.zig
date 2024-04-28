d47067301bd91637695839881db25380b9b22b7b
gabkhanfig <gabkhanfig@gmail.com>
2024-04-28 19:20:18 -0400

    Initial Class Implementation

---
+++ src/types/class.zig
@@ -0,0 +1,81 @@
+const std = @import("std");
+const assert = std.debug.assert;
+const expect = std.testing.expect;
+const root = @import("../root.zig");
+const RawValue = root.RawValue;
+const ValueTag = root.ValueTag;
+const String = root.String;
+const allocator = @import("../state/global_allocator.zig").allocator;
+
+/// Classes can make weak references, however if the class becomes a shared object, all existing weak references become invalid.
+pub const Class = extern struct {
+    const PTR_BITMASK: usize = 0x0000FFFFFFFFFFFF;
+    const MAX_CLASS_MEMBERS = std.math.maxInt(u16);
+
+    const Self = @This();
+
+    /// Points to a region of memory starting with a pointer to the runtime class info,
+    /// followed by all of the class members.
+    inner: usize,
+
+    pub fn deinit(self: *Self) void {
+        // TODO execute script deinit as well
+        for (self.membersInfo(), 0..) |info, i| {
+            self.valueAt(i).deinit(info.dataType);
+        }
+        const allocationSize = @sizeOf(*const RuntimeClassInfo) + (@sizeOf(RawValue) * self.membersInfo().len);
+        const ptr: *u8 = @ptrFromInt(self.inner);
+        allocator().free(ptr[0..allocationSize]);
+    }
+
+    /// Get an immutable reference to a class member.
+    /// Used with `valueTagAt()` to determine the active union member.
+    pub fn valueAt(self: *const Self, memberIndex: usize) *const RawValue {
+        assert(memberIndex < getRuntimeClassInfo(self).members.len);
+        const members: [*]const RawValue = @ptrCast(&@as([*]const RawValue, @ptrFromInt(self.inner))[1]); // Use index 1 to go past the RTTI
+        return &members[memberIndex];
+    }
+
+    /// Get a mutable reference to a class member.
+    /// Used with `valueTagAt()` to determine the active union member.
+    pub fn mutValueAt(self: *Self, memberIndex: usize) *RawValue {
+        assert(memberIndex < getRuntimeClassInfo(self).members.len);
+        const members: [*]RawValue = @ptrCast(&@as([*]RawValue, @ptrFromInt(self.inner))[1]); // Use index 1 to go past the RTTI
+        return &members[memberIndex];
+    }
+
+    /// Get the type of script value of the class at a specific member index.
+    /// Combines with `valueAt()` or `mutValueAt()` to determine the active union member.
+    pub fn valueTagAt(self: *const Self, memberIndex: usize) ValueTag {
+        const runtimeInfo = getRuntimeClassInfo(self);
+        assert(memberIndex < runtimeInfo.members.len);
+        return runtimeInfo.members[memberIndex].dataType;
+    }
+
+    /// Get the name of this class. This value is the same across multiple
+    /// instances of this class.
+    pub fn className(self: *const Self) *const String {
+        return &getRuntimeClassInfo(self).className;
+    }
+
+    /// Get a slice of the info of all the members of this class.
+    /// This is the member name and script data type.
+    pub fn membersInfo(self: *const Self) []const ClassMemberInfo {
+        return getRuntimeClassInfo(self).members;
+    }
+};
+
+pub fn getRuntimeClassInfo(class: *const Class) *const RuntimeClassInfo {
+    return @ptrFromInt(class.inner);
+}
+
+pub const RuntimeClassInfo = struct {
+    className: String,
+    members: []ClassMemberInfo,
+    // TODO onDeinit, interfaces
+};
+
+pub const ClassMemberInfo = extern struct {
+    name: String,
+    dataType: ValueTag,
+};
