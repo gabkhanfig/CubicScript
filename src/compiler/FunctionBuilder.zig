ed24f7b326d365288ead3c40311ef0c259845356
gabkhanfig <gabkhanfig@gmail.com>
2024-05-05 02:41:47 -0400

    Create FunctionBuilder.zig

---
+++ src/compiler/FunctionBuilder.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+const assert = std.debug.assert;
+const expect = std.testing.expect;
+const root = @import("../root.zig");
+const ValueTag = root.ValueTag;
+const String = root.String;
+const allocator = @import("../state/global_allocator.zig").allocator;
+const ArrayListUnmanaged = std.ArrayListUnmanaged;
+
+const Self = @This();
+
+name: String,
+fullyQualifiedName: String,
+_returnType: ValueTag = .None,
+_argTypes: ArrayListUnmanaged(ValueTag) = .{},
+_function: ?*anyopaque,
+
+pub fn deinit(self: *Self) void {
+    self.name.deinit();
+    self.fullyQualifiedName.deinit();
+    if (self._function) |function| {
+        _ = function;
+    } else {
+        self._argTypes.deinit(allocator());
+    }
+}
+
+pub const FunctionDeclaration = extern struct {
+    /// If is `.None` no value is returned. Equivalent to `void` return.
+    returnType: ValueTag,
+    /// Can be 0, meaning the function takes no arguments.
+    argCount: u8,
+    /// If is `null`, the function takes no arguments.
+    argTypes: ?[*]ValueTag, // This can be compressed into an array of u8
+    // TODO should argument names be stored?
+};
