14c999724a7a75a9a2ea71f28506caebc07e155c
gabkhanfig <gabkhanfig@gmail.com>
2024-05-21 00:27:35 -0400

    Test ScriptThread getId

--- c_impl/src/util/script_thread.zig
+++ c_impl/src/util/script_thread.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
+const expect = std.testing.expect;
 
 const c = struct {
-    extern fn cubs_thread_spawn(owner: ?*anyopaque) callconv(.C) ScriptThread;
+    extern fn cubs_thread_spawn(closeWithScript: bool) callconv(.C) ScriptThread;
     extern fn cubs_thread_close(thread: *ScriptThread) callconv(.C) void;
     extern fn cubs_thread_get_id(thread: *const ScriptThread) callconv(.C) c_int;
 };
@@ -12,13 +13,18 @@ pub const ScriptThread = extern struct {
     threadObj: *anyopaque,
     vtable: *const VTable,
 
+    /// Create an instance of an OS specific script thread. Implemention is defined in `script_thread.c`.
     pub fn spawn() Self {
-        return c.cubs_thread_spawn(null);
+        return c.cubs_thread_spawn(false);
     }
 
     /// Explicitly close the thread. If `self.vtable.close == null`, does nothing.
     pub fn close(self: *Self) void {
         c.cubs_thread_close(self);
+    }
+
+    pub fn getId(self: *const Self) u32 {
+        return @intCast(c.cubs_thread_get_id(self));
     }
 
     pub const VTable = extern struct {
@@ -30,5 +36,12 @@ pub const ScriptThread = extern struct {
     test spawn { // also tests close
         var thread = Self.spawn();
         defer thread.close();
+    }
+
+    test getId {
+        var thread = Self.spawn();
+        defer thread.close();
+
+        try expect(@as(u32, @intCast(std.Thread.getCurrentId())) != thread.getId());
     }
 };
