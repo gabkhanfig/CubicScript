12571ac898ef6031bc580ac9c435b3efad3213c3
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-05-30 16:53:58 -0400

    Update value_tag.h

--- c_impl/src/primitives/value_tag.h
+++ c_impl/src/primitives/value_tag.h
@@ -36,6 +36,7 @@ typedef enum CubsValueTag {
     cubsValueTagMapKeyIterator = 30,
     cubsValueTagConstMapPairIterator = 31,
     cubsValueTagMutMapPairIterator = 32,
+    cubsValueTagFuture = 33,
     // Enforce enum size is at least 32 bits, which is `int` on most platforms
     _CUBS_VALUE_TAG_MAX_VALUE = 0x7FFFFFFF,
 } CubsValueTag;
