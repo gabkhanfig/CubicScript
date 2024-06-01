9a7e007359d38c59fc1bd677fb360925383b7fa8
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-06-01 18:47:41 -0400

    Add GCC Support For Count Trailing Zeroes

--- c_impl/src/util/bitwise.h
+++ c_impl/src/util/bitwise.h
@@ -18,12 +18,28 @@ inline static bool countTrailingZeroes32(uint32_t* out, uint32_t mask) {
 #if defined(_WIN32) || defined(WIN32)
     _Static_assert(sizeof(uint32_t) == sizeof(unsigned long), "On Win32, uint32_t should have the same size as unsigned long");
     return _BitScanForward((unsigned long*)out, mask);
-#endif // WIN32 def
+#elif __GNUC__
+    if(mask == 0) {
+        return false;
+    }
+    *out = (uint32_t)__builtin_ctz(mask);
+    return true;
+#else
+_Static_assert(false, "count trailing zeroes 32 bit not implemented")
+#endif
 }
 
 inline static bool countTrailingZeroes64(uint32_t* out, uint64_t mask) {
 #if defined(_WIN32) || defined(WIN32)
-    _Static_assert(sizeof(uint32_t) == sizeof(unsigned long), "On Win32, uint32_t should have the same size as unsigned long");
+    _Static_assert(sizeof(uint64_t) == sizeof(unsigned long long), "On Win32, uint64_t should have the same size as unsigned long long");
     return _BitScanForward64((unsigned long*)out, mask);
-#endif // WIN32 def
+#elif __GNUC__
+    if(mask == 0) {
+        return false;
+    }
+    *out = (uint32_t)__builtin_ctzll(mask);
+    return true;
+#else
+_Static_assert(false, "count trailing zeroes 64 bit not implemented")
+#endif
 }
