357fffa52088604dadd9e65f931f76979db7829e
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-06-01 18:47:55 -0400

    Add GCC Support For Panic

--- c_impl/src/util/panic.c
+++ c_impl/src/util/panic.c
@@ -1,25 +1,31 @@
 #include "panic.h"
 #include "unreachable.h"
+#include <stdio.h>
 
 #if defined(_WIN32) || defined(WIN32)
 
 #define NOMINMAX
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#include <stdio.h>
+
+#endif // WIN32
 
 void cubs_panic(const char *message)
 {
     #if _DEBUG
     fprintf(stderr, "CubicScript panic:\n%s\n", message);
+    #if defined(_WIN32) || defined(WIN32)
     DebugBreak();
-    #else
+    #else __GNUC__
+    __builtin_trap();
+    #endif
+    #else // _DEBUG
+    #if defined(_WIN32) || defined(WIN32)
     MessageBox(NULL, message, "CubicScript Panic", MB_ICONERROR | MB_OK);
+    #endif //WIN32
+    fprintf(stderr, "CubicScript panic:\n%s\n", message);
     abort();
     #endif
     unreachable();
 }
-
-#endif // WIN32
-
 
