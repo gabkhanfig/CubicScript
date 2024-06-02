885bf69a07ed07a1f8fd4ed827f0dade06de6214
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-06-01 23:28:38 -0400

    Use stdatomic.h For Non-Windows

--- c_impl/src/util/atomic_ref_count.c
+++ c_impl/src/util/atomic_ref_count.c
@@ -3,6 +3,8 @@
 //#include<stdatomic.h>
 #if _MSC_VER
 #include <vcruntime_c11_atomic_support.h>
+#else
+#include <stdatomic.h>
 #endif
 
 #define MEMORY_ORDER_SEQ_CST 5
@@ -10,28 +12,27 @@
 void atomic_ref_count_init(AtomicRefCount* refCountToInit)
 {
 #if _MSC_VER
-  //__c11_atomic_init((volatile size_t*)&refCountToInit->count, 1);
-  volatile size_t* count = (volatile size_t*)&refCountToInit->count;
-  (*count) = 1;
+    volatile size_t* count = (volatile size_t*)&refCountToInit->count;
+    (*count) = 1;
 #else
-  __c11_atomic_init((_Atomic(size_t)*)& refCountToInit->count, 1);
+    atomic_init((atomic_size_t*)&refCountToInit->count, 1);
 #endif
 }
 
 void atomic_ref_count_add_ref(AtomicRefCount *self)
 {
 #if _MSC_VER
-  _Atomic_add_fetch64((volatile size_t*)&self->count, 1, MEMORY_ORDER_SEQ_CST);
+    _Atomic_add_fetch64((volatile size_t*)&self->count, 1, MEMORY_ORDER_SEQ_CST);
 #else
-  __c11_atomic_fetch_add((_Atomic(size_t)*)&self->count, 1, MEMORY_ORDER_SEQ_CST);
+    (void)atomic_fetch_add((atomic_size_t*)&self->count, 1);
 #endif
 }
 
 bool atomic_ref_count_remove_ref(AtomicRefCount *self)
 {
 #if _MSC_VER
-  return _Atomic_sub_fetch64((volatile size_t*)&self->count, 1, MEMORY_ORDER_SEQ_CST) == 1;
+    return _Atomic_sub_fetch64((volatile size_t*)&self->count, 1, MEMORY_ORDER_SEQ_CST) == 1;
 #else
-  return __c11_atomic_fetch_sub((_Atomic(size_t)*)&self->count, 1, MEMORY_ORDER_SEQ_CST) == 1;
+    return atomic_fetch_sub((atomic_size_t*)&self->count, 1) == 1;
 #endif
 }
