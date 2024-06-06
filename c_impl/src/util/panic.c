0fb8ce0045482b8c8401756188003c29583462a3
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-06-06 02:20:22 -0400

    Update panic.c

--- c_impl/src/util/panic.c
+++ c_impl/src/util/panic.c
@@ -8,24 +8,26 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-#endif // WIN32
-
-void cubs_panic(const char *message)
-{
+void cubs_panic(const char* message) {
     #if _DEBUG
     fprintf(stderr, "CubicScript panic:\n%s\n", message);
-    #if defined(_WIN32) || defined(WIN32)
     DebugBreak();
-    #else __GNUC__
-    __builtin_trap();
-    #endif
-    #else // _DEBUG
-    #if defined(_WIN32) || defined(WIN32)
+    #else 
     MessageBox(NULL, message, "CubicScript Panic", MB_ICONERROR | MB_OK);
-    #endif //WIN32
-    fprintf(stderr, "CubicScript panic:\n%s\n", message);
     abort();
     #endif
-    unreachable();
 }
+
+#elif __GNUC__
+
+void cubs_panic(const char* message) {
+    #if _DEBUG
+    fprintf(stderr, "CubicScript panic:\n%s\n", message);
+    __builtin_trap();
+    #else
+    abort();
+    #endif
+}
+
+#endif
 
