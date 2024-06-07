6f579a59561e12c3bc36f99e62695175ad37f87c
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-06-06 23:29:07 -0400

    GNU Malloc/Free Aligned

--- c_impl/src/util/os.c
+++ c_impl/src/util/os.c
@@ -2,19 +2,24 @@
 
 #if defined(_WIN32) || defined(WIN32)
 #include <malloc.h>
+#elif __GNUC__
+#include <stdlib.h>
 #endif
 
 void* _cubs_os_aligned_malloc(size_t len, size_t align) {
     #if defined(_WIN32) || defined(WIN32)
     // https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/aligned-malloc?view=msvc-170&viewFallbackFrom=vs-2019
-    void* mem = _aligned_malloc(len, (size_t)align);
-    return mem;
+    return _aligned_malloc(len, (size_t)align);
+    #elif __GNUC__
+    return aligned_alloc(align, len);
     #endif
 }
 
-void* _cubs_os_aligned_free(void *buf, size_t len, size_t align) {
+void _cubs_os_aligned_free(void *buf, size_t len, size_t align) {
     #if defined(_WIN32) || defined(WIN32)
     // https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/aligned-free?view=msvc-170
     _aligned_free(buf);
+    #elif __GNUC__
+    free(buf);
     #endif
 }
