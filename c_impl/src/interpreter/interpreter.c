f51905a308e1b31652db520497d5312f6088bfa3
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-06-16 22:34:18 -0400

    Small Refactor

--- c_impl/src/interpreter/interpreter.c
+++ c_impl/src/interpreter/interpreter.c
@@ -10,6 +10,62 @@
 
 static _Thread_local InterpreterRegisters threadLocalRegisters = {0};
 
+static void execute_load(size_t* ipIncrement, const Bytecode* bytecode) {
+    const OperandsLoadUnknown unknownOperands = *(const OperandsLoadUnknown*)&bytecode;
+
+    switch(unknownOperands.loadType) {
+        case LOAD_TYPE_IMMEDIATE: {
+            const OperandsLoadImmediate operands = *(const OperandsLoadImmediate*)&bytecode;
+
+            switch(operands.immediateType) {
+                case LOAD_IMMEDIATE_BOOL: {
+                    threadLocalRegisters.registers[operands.dst].boolean = operands.immediate != 0;
+                    threadLocalRegisters.registerValueTags[operands.dst] = cubsValueTagBool;
+                } break;
+                case LOAD_IMMEDIATE_INT: {
+                    threadLocalRegisters.registers[operands.dst].intNum = (int64_t)operands.immediate;
+                    threadLocalRegisters.registerValueTags[operands.dst] = cubsValueTagInt;
+                } break;
+                case LOAD_IMMEDIATE_FLOAT: {
+                    threadLocalRegisters.registers[operands.dst].floatNum = (double)operands.immediate;
+                    threadLocalRegisters.registerValueTags[operands.dst] = cubsValueTagFloat;
+                } break;
+                default: {
+                    unreachable();
+                } break;
+            }
+        } break;
+        case LOAD_TYPE_IMMEDIATE_LONG: {
+            const OperandsLoadImmediateLong operands = *(const OperandsLoadImmediateLong*)&bytecode;
+            assert(operands.immediateValueTag != cubsValueTagNone);
+            assert(operands.immediateValueTag != cubsValueTagBool && "Don't use 64 bit immediate load for booleans");
+
+            const size_t immediate = 
+                ((size_t)threadLocalRegisters.instructionPointer[1].value) |
+                (((size_t)threadLocalRegisters.instructionPointer[2].value) << 32);
+            threadLocalRegisters.registers[operands.dst].actualValue = immediate;     
+            threadLocalRegisters.registerValueTags[operands.dst] = operands.immediateValueTag;              
+            (*ipIncrement) += 2; // move instruction pointer further into the bytecode
+        } break;
+        case LOAD_TYPE_FROM_STACK: {
+            const OperandsLoadFromStack operands = *(const OperandsLoadFromStack*)&bytecode;
+            const CubsValueTag stackValueTag = cubs_interpreter_stack_tag_at(operands.offsetFromFrameStart);
+            assert(stackValueTag != cubsValueTagNone);
+
+            if(
+                stackValueTag == cubsValueTagBool 
+                || stackValueTag == cubsValueTagInt 
+                || stackValueTag == cubsValueTagFloat
+            ) {
+                memcpy((void*)&threadLocalRegisters.registers[operands.dst].actualValue, cubs_interpreter_stack_value_at(operands.offsetFromFrameStart), sizeof(size_t));
+            } else {
+                threadLocalRegisters.registers[operands.dst].ptr = cubs_interpreter_stack_value_at(operands.offsetFromFrameStart);
+            }
+            threadLocalRegisters.registerValueTags[operands.dst] = stackValueTag;
+        }
+    }
+}
+
 void cubs_interpreter_set_instruction_pointer(const Bytecode *newIp)
 {
     assert(newIp != NULL);
@@ -26,59 +82,7 @@ CubsFatalScriptError cubs_interpreter_execute_operation(const CubsProgram *progr
             fprintf(stderr, "nop :)\n");
         } break;
         case OpCodeLoad: {
-            const OperandsLoadUnknown unknownOperands = *(const OperandsLoadUnknown*)&bytecode;
-
-            switch(unknownOperands.loadType) {
-                case LOAD_TYPE_IMMEDIATE: {
-                    const OperandsLoadImmediate operands = *(const OperandsLoadImmediate*)&bytecode;
-
-                    switch(operands.immediateType) {
-                        case LOAD_IMMEDIATE_BOOL: {
-                            threadLocalRegisters.registers[operands.dst].boolean = operands.immediate != 0;
-                            threadLocalRegisters.registerValueTags[operands.dst] = cubsValueTagBool;
-                        } break;
-                        case LOAD_IMMEDIATE_INT: {
-                            threadLocalRegisters.registers[operands.dst].intNum = (int64_t)operands.immediate;
-                            threadLocalRegisters.registerValueTags[operands.dst] = cubsValueTagInt;
-                        } break;
-                        case LOAD_IMMEDIATE_FLOAT: {
-                            threadLocalRegisters.registers[operands.dst].floatNum = (double)operands.immediate;
-                            threadLocalRegisters.registerValueTags[operands.dst] = cubsValueTagFloat;
-                        } break;
-                        default: {
-                            unreachable();
-                        } break;
-                    }
-                } break;
-                case LOAD_TYPE_IMMEDIATE_LONG: {
-                    const OperandsLoadImmediateLong operands = *(const OperandsLoadImmediateLong*)&bytecode;
-                    assert(operands.immediateValueTag != cubsValueTagNone);
-                    assert(operands.immediateValueTag != cubsValueTagBool && "Don't use 64 bit immediate load for booleans");
-
-                    const size_t immediate = 
-                        ((size_t)threadLocalRegisters.instructionPointer[1].value) |
-                        (((size_t)threadLocalRegisters.instructionPointer[2].value) << 32);
-                    threadLocalRegisters.registers[operands.dst].actualValue = immediate;     
-                    threadLocalRegisters.registerValueTags[operands.dst] = operands.immediateValueTag;              
-                    ipIncrement += 2; // move instruction pointer further into the bytecode
-                } break;
-                case LOAD_TYPE_FROM_STACK: {
-                    const OperandsLoadFromStack operands = *(const OperandsLoadFromStack*)&bytecode;
-                    const CubsValueTag stackValueTag = cubs_interpreter_stack_tag_at(operands.offsetFromFrameStart);
-                    assert(stackValueTag != cubsValueTagNone);
-
-                    if(
-                        stackValueTag == cubsValueTagBool 
-                        || stackValueTag == cubsValueTagInt 
-                        || stackValueTag == cubsValueTagFloat
-                    ) {
-                        memcpy((void*)&threadLocalRegisters.registers[operands.dst].actualValue, cubs_interpreter_stack_value_at(operands.offsetFromFrameStart), sizeof(size_t));
-                    } else {
-                        threadLocalRegisters.registers[operands.dst].ptr = cubs_interpreter_stack_value_at(operands.offsetFromFrameStart);
-                    }
-                    threadLocalRegisters.registerValueTags[operands.dst] = stackValueTag;
-                }
-            }
+            execute_load(&ipIncrement, &bytecode);
         } break;
         default: {
             unreachable();
@@ -88,7 +92,7 @@ CubsFatalScriptError cubs_interpreter_execute_operation(const CubsProgram *progr
     return cubsFatalScriptErrorNone;
 }
 
-InterpreterRegister cubs_interpreter_register_value_at(size_t registerIndex){
+InterpreterRegister cubs_interpreter_register_value_at(size_t registerIndex) {
     return threadLocalRegisters.registers[registerIndex];
 }
 
