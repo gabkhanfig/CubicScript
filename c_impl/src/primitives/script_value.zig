3b66b2f116d46f07a76a43260f5ea08d7763a7dc
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-07-09 23:12:23 -0400

    Update script_value.zig

--- c_impl/src/primitives/script_value.zig
+++ c_impl/src/primitives/script_value.zig
@@ -29,6 +29,8 @@ pub const Vec2f = @import("vector/vector.zig").Vec2f;
 pub const Vec3f = @import("vector/vector.zig").Vec3f;
 pub const Vec4f = @import("vector/vector.zig").Vec4f;
 
+pub const CubsError = @import("error/error.zig").CubsError;
+
 pub const ValueTag = enum(c_int) {
     bool = 1,
     int = 2,
