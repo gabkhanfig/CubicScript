c4d93bb30fc391a64295a1a0cb7e8b2206cbb03f
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-07-12 20:14:51 -0400

    Add Unique and C Declarations to Module

--- c_impl/src/root.zig
+++ c_impl/src/root.zig
@@ -22,12 +22,15 @@ pub const Map = script_value.Map;
 pub const Option = script_value.Option;
 pub const Error = script_value.Error;
 pub const Result = script_value.Result;
+pub const Unique = script_value.Unique;
 pub const Vec2i = script_value.Vec2i;
 pub const Vec3i = script_value.Vec3i;
 pub const Vec4i = script_value.Vec4i;
 pub const Vec2f = script_value.Vec2f;
 pub const Vec3f = script_value.Vec3f;
 pub const Vec4f = script_value.Vec4f;
+
+pub const c = script_value.c;
 
 pub const sync_queue = @import("sync/sync_queue.zig");
 
