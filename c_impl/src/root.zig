fa4804b3613a9c6c57c7100e9d9c2a282ff171ef
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-07-14 19:19:12 -0400

    Add Shared To Module Exports

--- c_impl/src/root.zig
+++ c_impl/src/root.zig
@@ -23,6 +23,7 @@ pub const Option = script_value.Option;
 pub const Error = script_value.Error;
 pub const Result = script_value.Result;
 pub const Unique = script_value.Unique;
+pub const Shared = script_value.Unique;
 pub const Vec2i = script_value.Vec2i;
 pub const Vec3i = script_value.Vec3i;
 pub const Vec4i = script_value.Vec4i;
