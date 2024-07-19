754382971e87e10b569e6068d983cef02459c701
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-07-19 00:48:49 -0400

    Acquire Sync Ptr Primitives

--- c_impl/src/sync/sync_queue.zig
+++ c_impl/src/sync/sync_queue.zig
@@ -4,6 +4,10 @@ const Mutex = std.Thread.Mutex;
 const RwLock = std.Thread.RwLock;
 const ScriptMutex = @import("locks.zig").Mutex;
 const ScriptRwLock = @import("locks.zig").RwLock;
+const script_value = @import("../primitives//script_value.zig");
+const Unique = script_value.Unique;
+const Shared = script_value.Shared;
+const Weak = script_value.Weak;
 
 const c = struct {
     extern fn cubs_sync_queue_lock() callconv(.C) void;
@@ -11,6 +15,12 @@ const c = struct {
     extern fn cubs_sync_queue_unlock() callconv(.C) void;
     extern fn cubs_sync_queue_add_exclusive(object: SyncObject) callconv(.C) void;
     extern fn cubs_sync_queue_add_shared(object: SyncObject) callconv(.C) void;
+    extern fn cubs_sync_queue_unique_add_exclusive(unique: *script_value.c.CubsUnique) callconv(.C) void;
+    extern fn cubs_sync_queue_unique_add_shared(unique: *const script_value.c.CubsUnique) callconv(.C) void;
+    extern fn cubs_sync_queue_shared_add_exclusive(shared: *script_value.c.CubsShared) callconv(.C) void;
+    extern fn cubs_sync_queue_shared_add_shared(shared: *const script_value.c.CubsShared) callconv(.C) void;
+    extern fn cubs_sync_queue_weak_add_exclusive(weak: *script_value.c.CubsWeak) callconv(.C) void;
+    extern fn cubs_sync_queue_weak_add_shared(weak: *const script_value.c.CubsWeak) callconv(.C) void;
 };
 
 pub fn lock() void {
@@ -55,6 +65,69 @@ pub fn addScriptRwLockExclusive(rwlock: *ScriptRwLock) void {
 
 pub fn addScriptRwLockShared(rwlock: *const ScriptRwLock) void {
     addShared(SyncObject{ .ptr = @ptrCast(@constCast(rwlock)), .vtable = SCRIPT_RWLOCK_VTABLE });
+}
+
+pub fn addSyncPtrExclusive(ptr: anytype) void {
+    const ptrType = @TypeOf(ptr);
+    const typeInfo = @typeInfo(ptrType);
+    if (typeInfo != .Pointer) {
+        @compileError("Expected pointer type");
+    }
+    const ptrInfo = typeInfo.Pointer;
+    if (ptrInfo.size != .One) {
+        @compileError("Expected a one pointer");
+    }
+    if (ptrInfo.is_const) {
+        @compileError("Cannot get exclusive access to a const sync object");
+    }
+    if (!@hasDecl(ptrInfo.child, "SCRIPT_SELF_TAG")) {
+        @compileError("Expected script sync ptr type");
+    }
+    const scriptSelfTag: script_value.ValueTag = ptrInfo.child.SCRIPT_SELF_TAG;
+    switch (scriptSelfTag) {
+        .unique => {
+            c.cubs_sync_queue_unique_add_exclusive(ptr.asRawMut());
+        },
+        .shared => {
+            c.cubs_sync_queue_shared_add_exclusive(ptr.asRawMut());
+        },
+        .weak => {
+            c.cubs_sync_queue_weak_add_exclusive(ptr.asRawMut());
+        },
+        else => {
+            @compileError("Expected Unique, Shared, or Weak ptrs");
+        },
+    }
+}
+
+pub fn addSyncPtrShared(ptr: anytype) void {
+    const ptrType = @TypeOf(ptr);
+    const typeInfo = @typeInfo(ptrType);
+    if (typeInfo == .Pointer) {
+        @compileError("Expected pointer type");
+    }
+    const ptrInfo = typeInfo.Pointer;
+    if (ptrInfo.size != .One) {
+        @compileError("Expected a one pointer");
+    }
+    if (!@hasDecl(ptrInfo.child, "SCRIPT_SELF_TAG")) {
+        @compileError("Expected script sync ptr type");
+    }
+    const scriptSelfTag: script_value.ValueTag = ptrInfo.child.SCRIPT_SELF_TAG;
+    switch (scriptSelfTag) {
+        .unique => {
+            c.cubs_sync_queue_unique_add_shared(ptr.asRawMut());
+        },
+        .shared => {
+            c.cubs_sync_queue_shared_add_shared(ptr.asRawMut());
+        },
+        .weak => {
+            c.cubs_sync_queue_weak_add_shared(ptr.asRawMut());
+        },
+        else => {
+            @compileError("Expected Unique, Shared, or Weak ptrs");
+        },
+    }
 }
 
 pub const SyncObject = extern struct {
