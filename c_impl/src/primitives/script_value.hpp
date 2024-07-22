b0296384744a45f73754732a68576e9cda9fb454
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-07-21 21:21:52 -0400

    Type Context

--- c_impl/src/primitives/script_value.hpp
+++ c_impl/src/primitives/script_value.hpp
@@ -3,6 +3,8 @@
 namespace cubs {
     namespace detail {
         #include "script_value.h"
+        #include "primitives_context.h"
+        #include <type_traits>
     }
 
     enum class ValueTag : int {
@@ -44,5 +46,34 @@ namespace cubs {
         Mat4f = detail::cubsValueTagMat4f,
     };
 
+    /// See `autoTypeContext<T>()` to automatically create one for a type.
     using TypeContext = detail::CubsTypeContext;
+
+    namespace detail {
+        template<typename T, typename = void>
+        struct has_user_defined_context : std::false_type {};
+
+        template<typename T>
+        struct has_user_defined_context<T, std::enable_if_t<std::is_invocable_r<const TypeContext*, decltype(T::scriptTypeContext)>::value>>
+        : std::true_type {};
+
+        template<typename T>
+        constexpr bool has_user_defined_context_v = has_user_defined_context<T>::value;
+    }
+
+    template<typename T>
+    const TypeContext* autoTypeContext() {
+        using namespace detail;
+
+        if(std::is_same<T, bool>::value) {
+            return &CUBS_BOOL_CONTEXT;
+        } else if(std::is_same<T, int64_t>::value) {
+            return &CUBS_INT_CONTEXT;
+        } else if(std::is_same<T, double>::value) {
+            return &CUBS_FLOAT_CONTEXT;
+        } else if(has_user_defined_context_v<T>) {
+            return T::scriptTypeContext();
+        }
+    }
+
 }
