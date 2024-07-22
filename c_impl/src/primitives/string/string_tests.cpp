22a52d7990d2d0d36de368daeb774d9948cda912
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-07-22 02:28:31 -0400

    Added C++ String Tests

--- c_impl/src/primitives/string/string_tests.cpp
+++ c_impl/src/primitives/string/string_tests.cpp
@@ -2,3 +2,18 @@
 #include "../../doctest.h"
 
 using cubs::String;
+
+TEST_CASE("default constructor") {
+    String s;
+    CHECK_EQ(s.len(), 0);
+}
+
+TEST_CASE("string view constructor") {
+    String s = std::string_view("hello world!");
+    CHECK_EQ(s, std::string_view("hello world!"));
+}
+
+TEST_CASE("const char* constructor") {
+    String s = "hello world!";
+    CHECK_EQ(s, "hello world!");
+}
