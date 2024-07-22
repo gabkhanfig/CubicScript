e556e5c3ab6ef19ac4f70d24155836f0608a775a
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-07-22 01:31:19 -0400

    How did this compile originally

--- c_impl/src/util/mem.c
+++ c_impl/src/util/mem.c
@@ -6,20 +6,6 @@
 #include <stdlib.h>
 #endif
 
-#ifndef CUBS_USING_ZIG_ALLOCATOR
-
-void *cubs_malloc(size_t len, size_t align) {
-    void* mem = _cubs_os_aligned_malloc(len, (size_t)align);
-    assert(mem != NULL && "CubicScript failed to allocate memory");
-    return mem;
-}
-
-void cubs_free(void *buf, size_t len, size_t align) {
-    _cubs_os_aligned_free(buf, len, align);
-}
-
-#endif
-
 void* _cubs_raw_aligned_malloc(size_t len, size_t align) {
     #if defined(_WIN32) || defined(WIN32)
     // https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/aligned-malloc?view=msvc-170&viewFallbackFrom=vs-2019
@@ -37,3 +23,17 @@ void _cubs_raw_aligned_free(void *buf, size_t len, size_t align) {
     free(buf);
     #endif
 }
+
+#ifndef CUBS_USING_ZIG_ALLOCATOR
+
+void *cubs_malloc(size_t len, size_t align) {
+    void* mem = _cubs_raw_aligned_malloc(len, (size_t)align);
+    assert(mem != NULL && "CubicScript failed to allocate memory");
+    return mem;
+}
+
+void cubs_free(void *buf, size_t len, size_t align) {
+    _cubs_raw_aligned_free(buf, len, align);
+}
+
+#endif
