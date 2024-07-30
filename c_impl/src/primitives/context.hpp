9f54447b80a5ef0d8e7a53683005927c6c55b98b
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-07-30 00:55:28 -0400

    Remove Stuff From Global Namespace

--- c_impl/src/primitives/context.hpp
+++ c_impl/src/primitives/context.hpp
@@ -10,23 +10,25 @@ namespace cubs {
 #include <type_traits>
 #include "array/array.hpp"
 
-template<typename T, typename = void>
-struct _has_user_defined_context : std::false_type {};
-
-template<typename T>
-struct _has_user_defined_context<T, std::enable_if_t<std::is_invocable_r<const cubs::TypeContext*, decltype(T::scriptTypeContext)>::value>>
-: std::true_type {};
-
-template<typename T>
-constexpr bool _has_user_defined_context_v = _has_user_defined_context<T>::value;
-
-template <class T, template <class...> class Template>
-struct _is_specialization : std::false_type {};
-
-template <template <class...> class Template, class... Args>
-struct _is_specialization<Template<Args...>, Template> : std::true_type {};
-
 namespace cubs {
+    namespace detail {
+        template<typename T, typename = void>
+        struct _has_user_defined_context : std::false_type {};
+
+        template<typename T>
+        struct _has_user_defined_context<T, std::enable_if_t<std::is_invocable_r<const cubs::TypeContext*, decltype(T::scriptTypeContext)>::value>>
+        : std::true_type {};
+
+        template<typename T>
+        constexpr bool _has_user_defined_context_v = _has_user_defined_context<T>::value;
+
+        template <class T, template <class...> class Template>
+        struct _is_specialization : std::false_type {};
+
+        template <template <class...> class Template, class... Args>
+        struct _is_specialization<Template<Args...>, Template> : std::true_type {};
+    }
+
     template<typename T>
     inline const TypeContext* autoTypeContext() {
         if constexpr (std::is_same<T, bool>::value) {
@@ -35,7 +37,7 @@ namespace cubs {
             return &CUBS_INT_CONTEXT;
         } else if constexpr (std::is_same<T, double>::value) {
             return &CUBS_FLOAT_CONTEXT;
-        } else if constexpr (_has_user_defined_context_v<T>) {
+        } else if constexpr (detail::_has_user_defined_context_v<T>) {
             return T::scriptTypeContext();
         }
     }
