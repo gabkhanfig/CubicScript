a1fa89a2dff5cf2c983949350ababe88d5fd7020
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-08-05 17:07:12 -0400

    MutRef With Tests

--- c_impl/src/primitives/reference/reference.zig
+++ c_impl/src/primitives/reference/reference.zig
@@ -45,6 +45,48 @@ pub const CubsConstRef = extern struct {
     pub extern fn cubs_const_ref_hash(self: *const Self) callconv(.C) usize;
 };
 
+/// Can be freely cloned, as references do not use any form of GC.
+pub fn MutRef(comptime T: type) type {
+    return extern struct {
+        const Self = @This();
+        pub const ValueType = T;
+
+        ref: *T,
+        context: *const TypeContext = TypeContext.auto(T),
+
+        pub fn eql(self: *const Self, other: Self) bool {
+            return CubsMutRef.cubs_mut_ref_eql(self.asRaw(), other.asRaw());
+        }
+
+        pub fn eqlValue(self: *const Self, other: T) bool {
+            return CubsMutRef.cubs_mut_ref_eql_value(self.asRaw(), @ptrCast(&other));
+        }
+
+        pub fn hash(self: *const Self) usize {
+            return CubsMutRef.cubs_mut_ref_hash(self.asRaw());
+        }
+
+        pub fn asRaw(self: *const Self) *const CubsMutRef {
+            return @ptrCast(self);
+        }
+
+        pub fn asRawMut(self: *Self) *CubsMutRef {
+            return @ptrCast(self);
+        }
+    };
+}
+
+pub const CubsMutRef = extern struct {
+    ref: *anyopaque,
+    context: *const TypeContext,
+
+    const Self = @This();
+
+    pub extern fn cubs_mut_ref_eql(self: *const Self, other: *const Self) callconv(.C) bool;
+    pub extern fn cubs_mut_ref_eql_value(self: *const Self, other: *const anyopaque) callconv(.C) bool;
+    pub extern fn cubs_mut_ref_hash(self: *const Self) callconv(.C) usize;
+};
+
 test "const ref eql" {
     { // same ref
         const a: i64 = 11;
@@ -91,7 +133,73 @@ test "const ref eqlValue" {
 test "const ref hash" {
     const String = script_value.String;
 
-    const s = String.initUnchecked("hello world!");
+    var s = String.initUnchecked("hello world!");
+    defer s.deinit();
+
     const r = ConstRef(String){ .ref = &s };
+    try expect(s.hash() == r.hash());
+}
+
+test "mut ref mutate value" {
+    {
+        var a: i64 = 11;
+        const r1 = MutRef(i64){ .ref = &a };
+        r1.ref.* += 1;
+
+        try expect(r1.ref.* == 12);
+        try expect(a == 12);
+    }
+}
+
+test "mut ref eql" {
+    { // same ref
+        var a: i64 = 11;
+        const r1 = MutRef(i64){ .ref = &a };
+        const r2 = MutRef(i64){ .ref = &a };
+        try expect(r1.eql(r2));
+    }
+    { // different ref, same value
+        var a: i64 = 11;
+        const r1 = MutRef(i64){ .ref = &a };
+        var b: i64 = 11;
+        const r2 = MutRef(i64){ .ref = &b };
+        try expect(r1.eql(r2));
+    }
+    { // different ref, different value
+        var a: i64 = 11;
+        const r1 = MutRef(i64){ .ref = &a };
+        var b: i64 = 12;
+        const r2 = MutRef(i64){ .ref = &b };
+        try expect(!r1.eql(r2));
+    }
+}
+
+test "mut ref eqlValue" {
+    { // same value
+        var a: i64 = 11;
+        const r = MutRef(i64){ .ref = &a };
+        try expect(r.eqlValue(a));
+    }
+    { // same value
+        var a: i64 = 11;
+        const r = MutRef(i64){ .ref = &a };
+        const b: i64 = 11;
+        try expect(r.eqlValue(b));
+    }
+    { // different value
+        var a: i64 = 11;
+        const r = MutRef(i64){ .ref = &a };
+        const b: i64 = 12;
+        try expect(!r.eqlValue(b));
+    }
+}
+
+test "mut ref hash" {
+    const String = script_value.String;
+
+    var s = String.initUnchecked("hello world!");
+    defer s.deinit();
+
+    const r = MutRef(String){ .ref = &s };
     try expect(s.hash() == r.hash());
 }
