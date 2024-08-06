0b195d90ae9b829eaf0634f309697f675a8d88d9
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-08-06 19:51:59 -0400

    Update reference.h

--- c_impl/src/primitives/reference/reference.h
+++ c_impl/src/primitives/reference/reference.h
@@ -3,6 +3,10 @@
 #include "../../c_basic_types.h"
 #include "../script_value.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /// Is true if they share the same reference, or the different references hold the same value.
 bool cubs_const_ref_eql(const CubsConstRef* self, const CubsConstRef* other);
 
@@ -20,4 +24,8 @@ bool cubs_mut_ref_eql_value(const CubsMutRef* self, const void* other);
 
 /// Hashes the referenced value.
 size_t cubs_mut_ref_hash(const CubsMutRef* self);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
