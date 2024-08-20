c955f79ff46e0f70f5f846bddbb4326429427e9a
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-08-19 22:59:30 -0400

    Add Deinit And Bytecode Start

--- c_impl/src/interpreter/function_definition.c
+++ c_impl/src/interpreter/function_definition.c
@@ -1,3 +1,25 @@
 #include "function_definition.h"
 #include <stddef.h>
 #include "../primitives/context.h"
+#include "../platform/mem.h"
+#include "bytecode.h"
+#include "../program/program.h"
+
+void cubs_function_builder_deinit(FunctionBuilder *self)
+{
+    cubs_string_deinit(&self->fullyQualifiedName);
+    cubs_string_deinit(&self->name);
+    if(self->args.capacity > 0) {
+        cubs_free(self->args.optTypes, self->args.capacity * sizeof(const CubsTypeContext*), _Alignof(const CubsTypeContext*));
+    }
+    if(self->bytecodeCapacity > 0) {
+        cubs_free(self->bytecode, self->bytecodeCapacity * sizeof(Bytecode), _Alignof(Bytecode));
+    }
+    const FunctionBuilder zeroed = {0};
+    *self = zeroed;
+}
+
+const Bytecode *cubs_function_bytecode_start(const ScriptFunctionDefinitionHeader *header)
+{
+    return (const Bytecode*)&header[1];
+}
--- c_impl/src/interpreter/function_definition.h
+++ c_impl/src/interpreter/function_definition.h
@@ -6,26 +6,43 @@
 typedef struct CubsTypeContext;
 typedef struct CubsString CubsString;
 typedef struct Bytecode Bytecode;
+typedef struct CubsProgram CubsProgram;
 
 typedef struct ScriptFunctionArgTypesSlice {
     /// If NULL, the function take no arguments
     const CubsTypeContext** optTypes;
     /// If zero, the function take no arguments
-    size_t count;
+    size_t len;
+    size_t capacity;
 } ScriptFunctionArgTypesSlice;
 
+/// Must be deinitialized with `cubs_function_builder_deinit(...)` OR `cubs_function_builder_build(...)`.
+/// Both will appropriately free any used allocations.
 typedef struct {
+    CubsString fullyQualifiedName;
     CubsString name;
     /// Can be NULL, meaning function has a `void` or `none` return type
     const CubsTypeContext* optReturnType;
     ScriptFunctionArgTypesSlice args;
+    size_t stackSpaceRequired;
+    struct Bytecode* bytecode;
+    size_t bytecodeLen;
+    size_t bytecodeCapacity;
 } FunctionBuilder;
+
+void cubs_function_builder_deinit(FunctionBuilder* self);
+
+// Defined in `src/program/program.c`
+extern ScriptFunctionDefinitionHeader* cubs_function_builder_build(FunctionBuilder* self, CubsProgram* program);
 
 // TODO when the program allocates the header and bytecode, mprotect / VirutalProtect it to prevent malicious actors from overwriting bytecode
 
 typedef struct {
+    CubsString fullyQualifiedName;
     CubsString name;
     const CubsTypeContext* optReturnType;
     ScriptFunctionArgTypesSlice args;
     size_t bytecodeCount;
 } ScriptFunctionDefinitionHeader;
+
+const Bytecode* cubs_function_bytecode_start(const ScriptFunctionDefinitionHeader* header);
