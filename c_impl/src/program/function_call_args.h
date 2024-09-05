3139c379bcc86ad2979a395af5b6b096b6d92a6a
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-09-04 22:13:10 -0400

    Updated Function Call Documentation

--- c_impl/src/program/function_call_args.h
+++ c_impl/src/program/function_call_args.h
@@ -16,9 +16,14 @@ typedef struct CubsScriptFunctionCallArgs {
 extern "C" {
 #endif
 
+/// Pushes an argument into either the next script stack frame in order, or pushes to a C function call.
+/// Calling `cubs_function_push_arg(...)` without also eventually calling `cubs_function_call(...)` after
+/// pushing all arguments is undefined behaviour.
 void cubs_function_push_arg(CubsScriptFunctionCallArgs* self, void* arg, const struct CubsTypeContext* typeContext);
 
-/// Takes ownership of `self`, effectively deinitializing it. Stores the return value in `outReturn`.
+/// Takes ownership of `self`, effectively deinitializing it.
+/// NOTE - no actual deinitialization logic is necessary, just don't use the same CubsScriptFunctionCallArgs twice. 
+/// Stores the return value in `outReturn`.
 /// If the function has no return value, pass in `NULL` for `outReturn`.
 void cubs_function_call(CubsScriptFunctionCallArgs self, const struct CubsProgram* program, void* outReturn, const struct CubsTypeContext** outContext);
 
