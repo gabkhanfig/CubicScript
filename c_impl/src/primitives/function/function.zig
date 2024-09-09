ca4947dae85ef6bb9ef69e4a300b07e3e8e93ddf
Gabriel Khan <admin@Gabriels-MacBook-Pro.local>
2024-09-09 13:23:11 -0400

    tests for pushing and taking arguments with C functions

--- c_impl/src/primitives/function/function.zig
+++ c_impl/src/primitives/function/function.zig
@@ -83,8 +83,8 @@ pub const CFunctionHandler = extern struct {
     outReturn: FunctionReturn,
 
     pub const c = struct {
-        pub extern fn cubs_function_take_arg(self: Self, argIndex: usize, outArg: *anyopaque, outContext: **const TypeContext) callconv(.C) void;
-        pub extern fn cubs_function_return_set_value(self: *const Self, returnValue: *anyopaque, returnContext: *const TypeContext) callconv(.C) void;
+        pub extern fn cubs_function_take_arg(self: *const Self, argIndex: usize, outArg: *anyopaque, outContext: ?**const TypeContext) callconv(.C) void;
+        pub extern fn cubs_function_return_set_value(self: Self, returnValue: *anyopaque, returnContext: *const TypeContext) callconv(.C) void;
     };
 };
 
@@ -97,5 +97,98 @@ test "call C function" {
 
     const f = Function.initC(&Example.example);
     const a = f.startCall();
+    try expect(FunctionCallArgs.c.cubs_function_call(a, std.mem.zeroes(FunctionReturn)) == 0);
+}
+
+test "push C arg" {
+    const Example = struct {
+        fn example(_: CFunctionHandler) callconv(.C) c_int {
+            return 0;
+        }
+    };
+
+    const f = Function.initC(&Example.example);
+    var a = f.startCall();
+
+    var num: i64 = 5;
+
+    FunctionCallArgs.c.cubs_function_push_arg(&a, @ptrCast(&num), TypeContext.auto(i64));
+    try expect(FunctionCallArgs.c.cubs_function_call(a, std.mem.zeroes(FunctionReturn)) == 0);
+}
+
+test "take C arg" {
+    const Example = struct {
+        fn example(args: CFunctionHandler) callconv(.C) c_int {
+            var num: i64 = undefined;
+            var context: *const TypeContext = undefined;
+            CFunctionHandler.c.cubs_function_take_arg(&args, 0, @ptrCast(&num), &context);
+            expect(num == 5) catch {
+                std.debug.print("Test fail. Expected C function argument to be 5", .{});
+            };
+            return 0;
+        }
+    };
+
+    const f = Function.initC(&Example.example);
+    var a = f.startCall();
+
+    var num: i64 = 5;
+
+    FunctionCallArgs.c.cubs_function_push_arg(&a, @ptrCast(&num), TypeContext.auto(i64));
+    try expect(FunctionCallArgs.c.cubs_function_call(a, std.mem.zeroes(FunctionReturn)) == 0);
+}
+
+test "multiple arguments" {
+    const Example = struct {
+        fn example(args: CFunctionHandler) callconv(.C) c_int {
+            var arg0: String = undefined;
+            var context0: *const TypeContext = undefined;
+            var arg1: i64 = undefined;
+            var context1: *const TypeContext = undefined;
+            var arg2: String = undefined;
+            var context2: *const TypeContext = undefined;
+
+            CFunctionHandler.c.cubs_function_take_arg(&args, 0, @ptrCast(&arg0), &context0);
+            expect(context0 == TypeContext.auto(String)) catch {
+                std.debug.print("Expected argument 0 to be string context", .{});
+            };
+            expect(arg0.eqlSlice("hello world!")) catch {
+                std.debug.print("Test fail. Expected first C function argument to be \"hello world!\"", .{});
+            };
+
+            arg0.deinit();
+
+            CFunctionHandler.c.cubs_function_take_arg(&args, 1, @ptrCast(&arg1), &context1);
+            expect(context0 == TypeContext.auto(i64)) catch {
+                std.debug.print("Expected argument 1 to be string context", .{});
+            };
+            expect(arg1 == 10) catch {
+                std.debug.print("Test fail. Expected second C function argument to be 10", .{});
+            };
+
+            CFunctionHandler.c.cubs_function_take_arg(&args, 2, @ptrCast(&arg2), &context2);
+            expect(context2 == TypeContext.auto(String)) catch {
+                std.debug.print("Expected argument 2 to be string context", .{});
+            };
+            expect(arg0.eqlSlice("hello world!")) catch {
+                std.debug.print("Test fail. Expected first C function argument to be \"well hello to this truly glorious world!\"", .{});
+            };
+
+            arg2.deinit();
+
+            return 0;
+        }
+    };
+
+    const f = Function.initC(&Example.example);
+    var a = f.startCall();
+
+    var a0 = String.initUnchecked("hello world!");
+    var a1: i64 = 10;
+    var a2 = String.initUnchecked("well hello to this truly glorious world!");
+
+    FunctionCallArgs.c.cubs_function_push_arg(&a, @ptrCast(&a0), TypeContext.auto(String));
+    FunctionCallArgs.c.cubs_function_push_arg(&a, @ptrCast(&a1), TypeContext.auto(i64));
+    FunctionCallArgs.c.cubs_function_push_arg(&a, @ptrCast(&a2), TypeContext.auto(String));
     try expect(FunctionCallArgs.c.cubs_function_call(a, std.mem.zeroes(FunctionReturn)) == 0);
 }
