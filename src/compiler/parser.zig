dc30f24bdeb64b5e087abc7e06777200042f03d6
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-10-23 15:52:22 -0400

    Test For Whitespace

--- src/compiler/parser.zig
+++ src/compiler/parser.zig
@@ -36,3 +36,108 @@ test "parse keyword const" {
     try expect(parserIterPeek(parser) == c.TOKEN_NONE);
     try expect(parserIterNext(&parser) == c.TOKEN_NONE);
 }
+
+test "parse keyword const with whitespace characters" {
+    { // space
+        var parser = parserIterInit(" const");
+
+        try expect(parserIterPeek(parser) == c.CONST_KEYWORD);
+        try expect(parserIterNext(&parser) == c.CONST_KEYWORD);
+
+        try expect(parserIterPeek(parser) == c.TOKEN_NONE);
+        try expect(parserIterNext(&parser) == c.TOKEN_NONE);
+    }
+    { // tab
+        var parser = parserIterInit("   const");
+
+        try expect(parserIterPeek(parser) == c.CONST_KEYWORD);
+        try expect(parserIterNext(&parser) == c.CONST_KEYWORD);
+
+        try expect(parserIterPeek(parser) == c.TOKEN_NONE);
+        try expect(parserIterNext(&parser) == c.TOKEN_NONE);
+    }
+    { // tab sanity
+        var parser = parserIterInit("\tconst");
+
+        try expect(parserIterPeek(parser) == c.CONST_KEYWORD);
+        try expect(parserIterNext(&parser) == c.CONST_KEYWORD);
+
+        try expect(parserIterPeek(parser) == c.TOKEN_NONE);
+        try expect(parserIterNext(&parser) == c.TOKEN_NONE);
+    }
+    { // new line
+        var parser = parserIterInit("\nconst");
+
+        try expect(parserIterPeek(parser) == c.CONST_KEYWORD);
+        try expect(parserIterNext(&parser) == c.CONST_KEYWORD);
+
+        try expect(parserIterPeek(parser) == c.TOKEN_NONE);
+        try expect(parserIterNext(&parser) == c.TOKEN_NONE);
+    }
+    { // CRLF
+        var parser = parserIterInit("\r\nconst");
+
+        try expect(parserIterPeek(parser) == c.CONST_KEYWORD);
+        try expect(parserIterNext(&parser) == c.CONST_KEYWORD);
+
+        try expect(parserIterPeek(parser) == c.TOKEN_NONE);
+        try expect(parserIterNext(&parser) == c.TOKEN_NONE);
+    }
+    { // multiple spaces
+        var parser = parserIterInit("  const");
+
+        try expect(parserIterPeek(parser) == c.CONST_KEYWORD);
+        try expect(parserIterNext(&parser) == c.CONST_KEYWORD);
+
+        try expect(parserIterPeek(parser) == c.TOKEN_NONE);
+        try expect(parserIterNext(&parser) == c.TOKEN_NONE);
+    }
+    { // multiple tabs
+        const s = "       const";
+        var parser = parserIterInit(s);
+
+        try expect(parserIterPeek(parser) == c.CONST_KEYWORD);
+        try expect(parserIterNext(&parser) == c.CONST_KEYWORD);
+
+        std.debug.print("found peek token {}\n", .{parserIterPeek(parser)});
+
+        try expect(parserIterPeek(parser) == c.TOKEN_NONE);
+        try expect(parserIterNext(&parser) == c.TOKEN_NONE);
+    }
+    { // multiple tabs sanity
+        var parser = parserIterInit("\t\tconst");
+
+        try expect(parserIterPeek(parser) == c.CONST_KEYWORD);
+        try expect(parserIterNext(&parser) == c.CONST_KEYWORD);
+
+        try expect(parserIterPeek(parser) == c.TOKEN_NONE);
+        try expect(parserIterNext(&parser) == c.TOKEN_NONE);
+    }
+    { // multiple new line
+        var parser = parserIterInit("\n\nconst");
+
+        try expect(parserIterPeek(parser) == c.CONST_KEYWORD);
+        try expect(parserIterNext(&parser) == c.CONST_KEYWORD);
+
+        try expect(parserIterPeek(parser) == c.TOKEN_NONE);
+        try expect(parserIterNext(&parser) == c.TOKEN_NONE);
+    }
+    { // multiple CRLF
+        var parser = parserIterInit("\r\n\r\nconst");
+
+        try expect(parserIterPeek(parser) == c.CONST_KEYWORD);
+        try expect(parserIterNext(&parser) == c.CONST_KEYWORD);
+
+        try expect(parserIterPeek(parser) == c.TOKEN_NONE);
+        try expect(parserIterNext(&parser) == c.TOKEN_NONE);
+    }
+    { // combination
+        var parser = parserIterInit(" \t\n \r\n\r\n \n \t const");
+
+        try expect(parserIterPeek(parser) == c.CONST_KEYWORD);
+        try expect(parserIterNext(&parser) == c.CONST_KEYWORD);
+
+        try expect(parserIterPeek(parser) == c.TOKEN_NONE);
+        try expect(parserIterNext(&parser) == c.TOKEN_NONE);
+    }
+}
