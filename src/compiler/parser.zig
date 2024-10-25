1b6e3772dde4cab0371e3b1bedc9b165aa3658cd
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-10-25 18:24:20 -0400

    Test For Other Keywords

--- src/compiler/parser.zig
+++ src/compiler/parser.zig
@@ -201,3 +201,286 @@ test "parse const with characters after" {
         try expect(parserIterNext(&parser) == c.TOKEN_NONE);
     }
 }
+
+fn validateParseKeyword(comptime s: []const u8, comptime token: c_int) void {
+    { // normal
+        var parser = parserIterInit(s);
+
+        expect(parserIterPeek(parser) == token) catch unreachable;
+        expect(parserIterNext(&parser) == token) catch unreachable;
+
+        expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+        expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+    }
+    { // whitespace before
+        { // space
+            var parser = parserIterInit(" " ++ s);
+
+            expect(parserIterPeek(parser) == token) catch unreachable;
+            expect(parserIterNext(&parser) == token) catch unreachable;
+
+            expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+            expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+        }
+        { // tab
+            var parser = parserIterInit("   " ++ s);
+
+            expect(parserIterPeek(parser) == token) catch unreachable;
+            expect(parserIterNext(&parser) == token) catch unreachable;
+
+            expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+            expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+        }
+        { // tab sanity
+            var parser = parserIterInit("\t" ++ s);
+
+            expect(parserIterPeek(parser) == token) catch unreachable;
+            expect(parserIterNext(&parser) == token) catch unreachable;
+
+            expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+            expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+        }
+        { // new line
+            var parser = parserIterInit("\n" ++ s);
+
+            expect(parserIterPeek(parser) == token) catch unreachable;
+            expect(parserIterNext(&parser) == token) catch unreachable;
+
+            expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+            expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+        }
+        { // CRLF
+            var parser = parserIterInit("\r\n" ++ s);
+
+            expect(parserIterPeek(parser) == token) catch unreachable;
+            expect(parserIterNext(&parser) == token) catch unreachable;
+
+            expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+            expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+        }
+        { // multiple spaces
+            var parser = parserIterInit("  " ++ s);
+
+            expect(parserIterPeek(parser) == token) catch unreachable;
+            expect(parserIterNext(&parser) == token) catch unreachable;
+
+            expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+            expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+        }
+        { // multiple tabs
+            const str = "       " ++ s;
+            var parser = parserIterInit(str);
+
+            expect(parserIterPeek(parser) == token) catch unreachable;
+            expect(parserIterNext(&parser) == token) catch unreachable;
+
+            expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+            expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+        }
+        { // multiple tabs sanity
+            var parser = parserIterInit("\t\t" ++ s);
+
+            expect(parserIterPeek(parser) == token) catch unreachable;
+            expect(parserIterNext(&parser) == token) catch unreachable;
+
+            expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+            expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+        }
+        { // multiple new line
+            var parser = parserIterInit("\n\n" ++ s);
+
+            expect(parserIterPeek(parser) == token) catch unreachable;
+            expect(parserIterNext(&parser) == token) catch unreachable;
+
+            expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+            expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+        }
+        { // multiple CRLF
+            var parser = parserIterInit("\r\n\r\n" ++ s);
+
+            expect(parserIterPeek(parser) == token) catch unreachable;
+            expect(parserIterNext(&parser) == token) catch unreachable;
+
+            expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+            expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+        }
+        { // combination
+            var parser = parserIterInit(" \t\n \r\n\r\n \n \t " ++ s);
+
+            expect(parserIterPeek(parser) == token) catch unreachable;
+            expect(parserIterNext(&parser) == token) catch unreachable;
+
+            expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+            expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+        }
+    }
+    { // characters after
+        { // valid token
+            { // space
+                var parser = parserIterInit(s ++ " ");
+
+                expect(parserIterPeek(parser) == token) catch unreachable;
+                expect(parserIterNext(&parser) == token) catch unreachable;
+
+                expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+                expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+            }
+            { // new line
+                var parser = parserIterInit(s ++ "\n");
+
+                expect(parserIterPeek(parser) == token) catch unreachable;
+                expect(parserIterNext(&parser) == token) catch unreachable;
+
+                expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+                expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+            }
+            { // tab
+                var parser = parserIterInit(s ++ "\t");
+
+                expect(parserIterPeek(parser) == token) catch unreachable;
+                expect(parserIterNext(&parser) == token) catch unreachable;
+
+                expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+                expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+            }
+            { // carriage return
+                var parser = parserIterInit(s ++ "\r");
+
+                expect(parserIterPeek(parser) == token) catch unreachable;
+                expect(parserIterNext(&parser) == token) catch unreachable;
+            }
+            { // comma
+                var parser = parserIterInit(s ++ ",");
+
+                expect(parserIterPeek(parser) == token) catch unreachable;
+                expect(parserIterNext(&parser) == token) catch unreachable;
+            }
+            { // period
+                var parser = parserIterInit(s ++ ".");
+
+                expect(parserIterPeek(parser) == token) catch unreachable;
+                expect(parserIterNext(&parser) == token) catch unreachable;
+            }
+            { // semicolon
+                var parser = parserIterInit(s ++ ";");
+
+                expect(parserIterPeek(parser) == token) catch unreachable;
+                expect(parserIterNext(&parser) == token) catch unreachable;
+            }
+        }
+        { // invalid token
+            var parser = parserIterInit(s ++ "t");
+
+            expect(parserIterPeek(parser) == c.TOKEN_NONE) catch unreachable;
+            expect(parserIterNext(&parser) == c.TOKEN_NONE) catch unreachable;
+        }
+    }
+}
+
+test "mut" {
+    validateParseKeyword("mut", c.MUT_KEYWORD);
+}
+
+test "return" {
+    validateParseKeyword("return", c.RETURN_KEYWORD);
+}
+
+test "fn" {
+    validateParseKeyword("fn", c.FN_KEYWORD);
+}
+
+test "pub" {
+    validateParseKeyword("pub", c.PUB_KEYWORD);
+}
+
+test "if" {
+    validateParseKeyword("if", c.IF_KEYWORD);
+}
+
+test "else" {
+    validateParseKeyword("else", c.ELSE_KEYWORD);
+}
+
+test "switch" {
+    validateParseKeyword("switch", c.SWITCH_KEYWORD);
+}
+
+test "while" {
+    validateParseKeyword("while", c.WHILE_KEYWORD);
+}
+
+test "for" {
+    validateParseKeyword("for", c.FOR_KEYWORD);
+}
+
+test "break" {
+    validateParseKeyword("break", c.BREAK_KEYWORD);
+}
+
+test "continue" {
+    validateParseKeyword("continue", c.CONTINUE_KEYWORD);
+}
+
+test "struct" {
+    validateParseKeyword("struct", c.STRUCT_KEYWORD);
+}
+
+test "enum" {
+    validateParseKeyword("enum", c.ENUM_KEYWORD);
+}
+
+test "union" {
+    validateParseKeyword("union", c.UNION_KEYWORD);
+}
+
+test "sync" {
+    validateParseKeyword("sync", c.SYNC_KEYWORD);
+}
+
+test "unsafe" {
+    validateParseKeyword("unsafe", c.UNSAFE_KEYWORD);
+}
+
+test "true" {
+    validateParseKeyword("true", c.TRUE_KEYWORD);
+}
+
+test "false" {
+    validateParseKeyword("false", c.FALSE_KEYWORD);
+}
+
+test "bool" {
+    validateParseKeyword("bool", c.BOOL_KEYWORD);
+}
+
+test "int" {
+    validateParseKeyword("int", c.INT_KEYWORD);
+}
+
+test "float" {
+    validateParseKeyword("float", c.FLOAT_KEYWORD);
+}
+
+test "str" {
+    validateParseKeyword("str", c.STR_KEYWORD);
+}
+
+test "char" {
+    validateParseKeyword("char", c.CHAR_KEYWORD);
+}
+
+test "import" {
+    validateParseKeyword("import", c.IMPORT_KEYWORD);
+}
+
+test "mod" {
+    validateParseKeyword("mod", c.MOD_KEYWORD);
+}
+
+test "and" {
+    validateParseKeyword("and", c.AND_KEYWORD);
+}
+
+test "or" {
+    validateParseKeyword("or", c.OR_KEYWORD);
+}
