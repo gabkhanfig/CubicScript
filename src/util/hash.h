bcb629d99577091ce28c491127621b69cf99afaa
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-11-16 19:43:06 -0500

    Make Accessible Bytes Hashing Function

--- src/util/hash.h
+++ src/util/hash.h
@@ -14,6 +14,50 @@ inline static size_t cubs_combine_hash(size_t a, size_t b) {
     return h;
 }
 
+inline static size_t bytes_hash(const void* ptr, size_t len) {
+    // murmurHash64A
+    const size_t seed = cubs_hash_seed();
+    const uint64_t m = 0xc6a4a7935bd1e995LLU;
+    const int r = 47;
+
+    uint64_t h = seed ^ (len * m);
+
+    const uint64_t * data = (const uint64_t *)ptr;
+    const uint64_t * end = (len >> 3) + data;
+
+    while(data != end)
+    {
+        uint64_t k = *data++;
+
+        k *= m; 
+        k ^= k >> r; 
+        k *= m; 
+        
+        h ^= k;
+        h *= m; 
+    }
+
+    const unsigned char * data2 = (const unsigned char *)data;
+
+    switch(len & 7)
+    {
+    case 7: h ^= (uint64_t)(data2[6]) << 48;
+    case 6: h ^= (uint64_t)(data2[5]) << 40;
+    case 5: h ^= (uint64_t)(data2[4]) << 32;
+    case 4: h ^= (uint64_t)(data2[3]) << 24;
+    case 3: h ^= (uint64_t)(data2[2]) << 16;
+    case 2: h ^= (uint64_t)(data2[1]) << 8;
+    case 1: h ^= (uint64_t)(data2[0]);
+            h *= m;
+    };
+    
+    h ^= h >> r;
+    h *= m;
+    h ^= h >> r;
+
+    return h;
+}
+
 typedef struct CubsHashGroupBitmask {
     size_t value;
 } CubsHashGroupBitmask;
