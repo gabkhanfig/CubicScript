4a9f1ab3a0e568361e782779079a8b2994caac12
Gabriel Khan <admin@Gabriels-MacBook-Pro.local>
2024-11-18 13:22:20 -0500

    make initial ast structures

--- src/compiler/ast.h
+++ src/compiler/ast.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include <stddef.h>
+#include "../primitives/string/string_slice.h"
+
+struct CubsProgram;
+struct Ast;
+
+typedef size_t(*AstNodeProcess)(const void* self, struct Ast* ast);
+typedef CubsStringSlice(*AstNodeToString)(const void* self);
+
+typedef struct AstNodeVTable {
+    AstNodeProcess process;
+    AstNodeToString toString;
+} AstNodeVTable;
+
+typedef struct AstNode {
+    void* ptr;
+    const AstNodeVTable* vtable;
+} AstNode;
+
+typedef struct Ast {
+    struct CubsProgram* program;
+} Ast;
