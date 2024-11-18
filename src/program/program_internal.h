32d18309d2b29069776c70787b9a972f94c8bdde
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-11-17 22:05:27 -0500

    move useful testing functionality of program

--- src/program/program_internal.h
+++ src/program/program_internal.h
@@ -1,9 +1,19 @@
 #pragma once
 
 #include "program.h"
+#include "protected_arena.h"
+#include "../sync/locks.h"
+#include "function_map.h"
+
+typedef struct {
+    ProtectedArena arena;
+    CubsProgramContext context;
+    CubsMutex contextMutex;
+    FunctionMap functionMap;
+} ProgramInner;
 
 /// If `params.context == NULL`, uses the default context. Otherwise, copies `params.context`, taking ownership of it, 
 /// and setting the original reference to `NULL`, ie. `params.context->ptr = NULL`.
-extern CubsProgram cubs_program_init(CubsProgramInitParams params);
+CubsProgram cubs_program_init(CubsProgramInitParams params);
 
-extern void _cubs_internal_program_runtime_error(const CubsProgram* self, CubsProgramRuntimeError err, const char* message, size_t messageLength);
+void _cubs_internal_program_runtime_error(const CubsProgram* self, CubsProgramRuntimeError err, const char* message, size_t messageLength);
