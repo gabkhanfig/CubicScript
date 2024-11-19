60266bfb89951be70a101d7a8cb44aaebd0e51c2
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-11-18 23:37:14 -0500

    Declare Ast Functions

--- src/compiler/ast.c
+++ src/compiler/ast.c
@@ -1,1 +1,22 @@
 #include "ast.h"
+#include <stdio.h>
+#include "../program/program.h"
+#include "../program/program_internal.h"
+
+Ast cubs_ast_init(TokenIter iter, CubsProgram* program)
+{
+    Ast ast = {.program = program};
+    return ast;
+}
+
+void cubs_ast_deinit(Ast *self)
+{
+}
+
+void cubs_ast_codegen(const Ast *self)
+{
+}
+
+void cubs_ast_print(const Ast *self)
+{
+}
--- src/compiler/ast.h
+++ src/compiler/ast.h
@@ -2,14 +2,17 @@
 
 #include <stddef.h>
 #include "../primitives/string/string_slice.h"
+#include "tokenizer.h"
 
 struct CubsProgram;
 struct Ast;
 
+typedef void (*AstNodeDeinit)(void* self);
 typedef size_t(*AstNodeProcess)(const void* self, struct CubsProgram* program);
 typedef CubsStringSlice(*AstNodeToString)(const void* self);
 
 typedef struct AstNodeVTable {
+    AstNodeDeinit deinit;
     AstNodeProcess process;
     AstNodeToString toString;
 } AstNodeVTable;
@@ -22,3 +25,11 @@ typedef struct AstNode {
 typedef struct Ast {
     struct CubsProgram* program;
 } Ast;
+
+Ast cubs_ast_init(TokenIter iter, struct CubsProgram* program);
+
+void cubs_ast_deinit(Ast* self);
+
+void cubs_ast_codegen(const Ast* self);
+
+void cubs_ast_print(const Ast* self);
