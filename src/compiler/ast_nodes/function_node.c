edc7a52483750dbed53290e790fe4861bbc6adfc
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-11-22 13:54:31 -0500

    Function Node Stuff

--- src/compiler/ast_nodes/function_node.c
+++ src/compiler/ast_nodes/function_node.c
@@ -1,6 +1,27 @@
 #include "function_node.h"
 
+static void function_node_deinit(FunctionNode* self) {
+    ast_node_array_deinit(&self->items);
+    cubs_free(self, sizeof(FunctionNode), _Alignof(FunctionNode));
+}
+
+static CubsStringSlice function_node_to_string(const FunctionNode* self) {
+    return (CubsStringSlice){0};
+}
+
+static AstNodeVTable function_node_vtable = {
+    .deinit = (AstNodeDeinit)&function_node_deinit,
+    .process = NULL,
+    .toString = (AstNodeToString)&function_node_to_string,
+};
+
+
 AstNode cubs_function_node_init(const TokenIter *iter)
 {
-    return (AstNode){0};
+    assert(iter->current == FN_KEYWORD);
+    FunctionNode* self = (FunctionNode*)cubs_malloc(sizeof(FunctionNode), _Alignof(FunctionNode));
+    self->items = (AstNodeArray){0};
+
+    const AstNode node = {.ptr = (void*)self, .vtable = &function_node_vtable};
+    return node;
 }
