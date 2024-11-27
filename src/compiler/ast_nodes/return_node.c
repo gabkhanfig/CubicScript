92f580494cf14c9fceb09901db78e76234b7fb75
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-11-27 16:53:31 -0500

    build return statement

--- src/compiler/ast_nodes/return_node.c
+++ src/compiler/ast_nodes/return_node.c
@@ -2,6 +2,9 @@
 #include "../../util/panic.h"
 #include "../../platform/mem.h"
 #include <assert.h>
+#include "../../interpreter/function_definition.h"
+#include "../../interpreter/interpreter.h"
+#include "../../interpreter/operations.h"
 
 static void return_node_deinit(ReturnNode* self) {
     cubs_free(self, sizeof(ReturnNode), _Alignof(ReturnNode));
@@ -11,11 +14,29 @@ static CubsStringSlice return_node_to_string(const ReturnNode* self) {
     return (CubsStringSlice){0};
 }
 
+static void return_node_build_function(const ReturnNode* self, FunctionBuilder* builder, const void* reserveStackAssignments) {  
+    if(self->hasReturn) {
+        const Bytecode bytecode = operands_make_return(false, 0);
+        cubs_function_builder_push_bytecode(builder, bytecode);
+    } else {
+        const uint16_t returnSrc = 0; // TODO get actual src
+        assert(self->retInfo == INT_LITERAL);
+
+        Bytecode loadImmediateLong[2];
+        operands_make_load_immediate_long(loadImmediateLong, cubsValueTagInt, returnSrc, self->retValue.intLiteral);
+
+        const Bytecode returnBytecode =  operands_make_return(true, returnSrc);
+        
+        cubs_function_builder_push_bytecode_many(builder, loadImmediateLong, 2);
+        cubs_function_builder_push_bytecode(builder, returnBytecode);
+    }
+}
+
 static AstNodeVTable return_node_vtable = {
     .deinit = (AstNodeDeinit)&return_node_deinit,
     .compile = NULL,
     .toString = (AstNodeToString)&return_node_to_string,
-    .buildFunction = NULL,
+    .buildFunction = (AstNodeBuildFunction)&return_node_build_function,
 };
 
 AstNode cubs_return_node_init(TokenIter *iter)
