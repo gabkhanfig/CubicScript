2ebfc3f3ab4536eb731381e0d47bffb7617137cb
Gabriel Khan <admin@Gabriels-MacBook-Pro.local>
2024-11-28 19:21:22 -0500

    add return type for function ast node and some parsing

--- src/compiler/ast_nodes/function_node.c
+++ src/compiler/ast_nodes/function_node.c
@@ -21,7 +21,41 @@ AstNode cubs_function_node_init(TokenIter *iter)
 {
     assert(iter->current == FN_KEYWORD);
     FunctionNode* self = (FunctionNode*)cubs_malloc(sizeof(FunctionNode), _Alignof(FunctionNode));
-    self->items = (AstNodeArray){0};
+    *self = (FunctionNode){0}; // 0 initialize everything. Means no return type by default
+
+    { // function name
+        const Token token = cubs_token_iter_next(iter);
+        assert(token == IDENTIFIER && "Identifier must occur after fn keyword");
+        self->functionName = iter->currentMetadata.identifier;
+    }
+
+    assert(cubs_token_iter_next(iter) == LEFT_PARENTHESES_SYMBOL);
+    assert(cubs_token_iter_next(iter) == RIGHT_PARENTHESES_SYMBOL); // no arguments for now
+
+    { // return type
+        Token token = cubs_token_iter_next(iter);
+        if(token != LEFT_BRACKET_SYMBOL) { // has return type            
+            if(token == INT_LITERAL) {
+                self->retInfo.retTag = functionReturnToken;
+                self->retInfo.retType.token = INT_KEYWORD;
+            }
+            if(token == IDENTIFIER) {
+                assert("Cannot handle identifier returns yet");
+            } else {
+                assert("Cannot handle other stuff for function return yet");
+            }
+
+            token = cubs_token_iter_next(iter); // left bracket should follow after token
+            assert(token == LEFT_BRACKET_SYMBOL); 
+        }
+        // TODO more complex return types, for now just nothing or ints           
+    }
+
+    { // statements
+        Token token = cubs_token_iter_next(iter);
+        assert(token == RETURN_KEYWORD);
+    }
+
 
     const AstNode node = {.ptr = (void*)self, .vtable = &function_node_vtable};
     return node;
--- src/compiler/ast_nodes/function_node.h
+++ src/compiler/ast_nodes/function_node.h
@@ -1,9 +1,27 @@
 #include "../ast.h"
 #include "ast_node_array.h"
 
+enum FunctionReturnEType {
+    functionReturnNone = 0,
+    functionReturnToken,
+    /// struct identifier for example
+    functionReturnIdentifier,
+};
+
+union FunctionReturnUType {
+    Token token;
+    CubsStringSlice identifier;
+};
+
+typedef struct FunctionReturnType {
+    enum FunctionReturnEType retTag;
+    union FunctionReturnUType retType;
+} FunctionReturnType;
+
 typedef struct FunctionNode {
     CubsStringSlice functionName;
     AstNodeArray items;
+    FunctionReturnType retInfo;
 } FunctionNode;
 
 AstNode cubs_function_node_init(TokenIter* iter);
