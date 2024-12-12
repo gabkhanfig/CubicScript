84905121f0985ddadd5f90b6940c430486c2d0f8
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-12-11 19:09:21 -0500

    Stack Assignment Implementation

--- src/compiler/stack_assignment.c
+++ src/compiler/stack_assignment.c
@@ -1,1 +1,81 @@
 #include "stack_assignment.h"
+#include "../platform/mem.h"
+#include <assert.h>
+
+void cubs_stack_assignment_deinit(StackAssignment *self)
+{
+    for(size_t i = 0; i < self->len; i++) {
+        cubs_string_deinit(&self->names[i]);
+    }
+
+    if(self->capacity > 0) {
+        assert(self->names != NULL);
+        assert(self->positions != NULL);
+
+        cubs_free((void*)self->names, self->capacity * sizeof(CubsString), _Alignof(CubsString));
+        cubs_free((void*)self->positions, self->capacity * sizeof(uint16_t), _Alignof(uint16_t));
+    }
+
+    *self = (StackAssignment){0};
+}
+
+uint16_t cubs_stack_assignment_push(StackAssignment *self, CubsString name, size_t sizeOfType)
+{
+    assert(self->requiredFrameSize < UINT16_MAX);
+    const uint16_t position = (uint16_t)self->requiredFrameSize;
+
+    size_t slotsForVariable = 1;
+    if(sizeOfType > 8) {
+        for(size_t i = 1; i < (sizeOfType / 8); i++) {
+            slotsForVariable++; // TODO probably could do this in a one liner
+        }
+    }
+    
+    self->requiredFrameSize += slotsForVariable;
+    assert(self->requiredFrameSize <= UINT16_MAX);
+
+    if(self->len == self->capacity) {
+        const size_t newCapacity = self->capacity == 0 ? 2 : self->capacity << 1;
+
+        CubsString* newNames = (CubsString*)cubs_malloc(newCapacity * sizeof(CubsString), _Alignof(CubsString));
+        uint16_t* newPositions = (uint16_t*)cubs_malloc(newCapacity * sizeof(uint16_t), _Alignof(uint16_t));
+
+        if(self->names != NULL) {
+            assert(self->positions != NULL);
+
+            for(uint32_t i = 0; i < self->len; i++) {
+                newNames[i] = self->names[i];
+                newPositions[i] = self->positions[i];
+            }
+
+            cubs_free((void*)self->names, self->capacity * sizeof(CubsString), _Alignof(CubsString));
+            cubs_free((void*)self->positions, self->capacity * sizeof(uint16_t), _Alignof(uint16_t));
+        } else {
+            // Validation
+            assert(self->positions == NULL);
+        }
+
+        self->names = newNames;
+        self->positions = newPositions;
+    }
+
+    self->names[self->len] = name;
+    self->positions[self->len] = position;
+    self->len += 1;
+
+    return position;
+}
+
+uint16_t cubs_stack_assignment_find(const StackAssignment *self, const CubsString *name)
+{
+    // TODO does this need optimization?
+
+    for(size_t i = 0; i < self->len; i++) {
+        if(cubs_string_eql(&self->names[i], name)) {
+            return self->positions[i];
+        }
+    }
+
+    assert(false && "Failed to find variable in stack assignment");
+    return 0;
+}
--- src/compiler/stack_assignment.h
+++ src/compiler/stack_assignment.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+#include "../primitives/string/string.h"
+
+/// Zero initialize.
+/// Stores stack positions of all variables within a stack frame
+typedef struct StackAssignment {
+    CubsString* names;
+    uint16_t* positions;
+    size_t len;
+    /// How many slots is required to store all of the variables
+    /// for this stack frame
+    size_t requiredFrameSize;
+    size_t capacity;
+} StackAssignment;
+
+void cubs_stack_assignment_deinit(StackAssignment* self);
+
+/// Takes ownership of `name`.
+/// Determines the position of the variable within the stack frame given the size of the type.
+/// Returns the position, but can be ignored.
+uint16_t cubs_stack_assignment_push(StackAssignment* self, CubsString name, size_t sizeOfType);
+
+uint16_t cubs_stack_assignment_find(const StackAssignment* self, const CubsString* name);
