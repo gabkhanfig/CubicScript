822a8a78040959ee55879bebdefdb0c64761bef0
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-12-13 17:07:36 -0500

    Multi Slot Variables

--- src/compiler/stack_variables.zig
+++ src/compiler/stack_variables.zig
@@ -39,6 +39,8 @@ test "stack assignment one variable one slot" {
     try expect(assignment.len == 1);
     // should occupy slot 0
     try expect(c.cubs_stack_assignment_find(&assignment, variableName) == 0);
+
+    try expect(assignment.requiredFrameSize == 1);
 }
 
 test "stack assignment two variables one slot each" {
@@ -58,6 +60,7 @@ test "stack assignment two variables one slot each" {
         try expect(assignment.len == 1);
         // should occupy slot 0
         try expect(c.cubs_stack_assignment_find(&assignment, v1) == 0);
+        try expect(assignment.requiredFrameSize == 1);
     }
     { // variable 2
         try expect(c.cubs_stack_assignment_push(&assignment, v2, size));
@@ -66,5 +69,54 @@ test "stack assignment two variables one slot each" {
         // should occupy slot 0
         try expect(c.cubs_stack_assignment_find(&assignment, v1) == 0);
         try expect(c.cubs_stack_assignment_find(&assignment, v2) == 1);
+        try expect(assignment.requiredFrameSize == 2);
+    }
+}
+
+test "stack assignment one variable multi slot" {
+    var assignment = StackVariablesAssignment{};
+    defer c.cubs_stack_assignment_deinit(&assignment);
+
+    try expect(assignment.len == 0);
+
+    const variableName = CubsStringSlice{ .str = "hello".ptr, .len = 5 };
+    // Use size of i64 because it only occupies one stack slot
+    const size = @sizeOf(CubsString);
+    try expect(c.cubs_stack_assignment_push(&assignment, variableName, size));
+
+    try expect(assignment.len == 1);
+    // should occupy slot 0
+    try expect(c.cubs_stack_assignment_find(&assignment, variableName) == 0);
+
+    try expect(assignment.requiredFrameSize == 4);
+}
+
+test "stack assignment two variables multi slot each" {
+    var assignment = StackVariablesAssignment{};
+    defer c.cubs_stack_assignment_deinit(&assignment);
+
+    try expect(assignment.len == 0);
+
+    const v1 = CubsStringSlice{ .str = "hello".ptr, .len = 5 };
+    const v2 = CubsStringSlice{ .str = "world".ptr, .len = 5 };
+    // Use size of i64 because it only occupies one stack slot
+    const size = @sizeOf(CubsString);
+
+    { // variable 1
+        try expect(c.cubs_stack_assignment_push(&assignment, v1, size));
+
+        try expect(assignment.len == 1);
+        // should occupy slot 0
+        try expect(c.cubs_stack_assignment_find(&assignment, v1) == 0);
+        try expect(assignment.requiredFrameSize == 4);
+    }
+    { // variable 2
+        try expect(c.cubs_stack_assignment_push(&assignment, v2, size));
+
+        try expect(assignment.len == 2);
+        // should occupy slot 0
+        try expect(c.cubs_stack_assignment_find(&assignment, v1) == 0);
+        try expect(c.cubs_stack_assignment_find(&assignment, v2) == 4);
+        try expect(assignment.requiredFrameSize == 8);
     }
 }
