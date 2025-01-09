96e14938090db152c7571cbd4ae48cd95c1f0252
Gabriel Khan <admin@Gabriels-MacBook-Pro.local>
2025-01-09 11:06:55 -0500

    add support for parsing variables in expressions

--- src/compiler/ast_nodes/expression_value.c
+++ src/compiler/ast_nodes/expression_value.c
@@ -3,31 +3,47 @@
 #include "../../platform/mem.h"
 #include "../../interpreter/function_definition.h"
 
-ExprValue cubs_parse_expression(
-    TokenIter* iter, 
-    struct StackVariablesArray* variables, 
-    bool hasDestination, 
-    size_t destinationVariableIndex
-) {
-    const TokenType firstToken = cubs_token_iter_next(iter);
-    // If the expression is just ";", for now return zeroed integer
-    // TODO handle true empty value
-    assert(firstToken != SEMICOLON_SYMBOL);
+/// Steps the iterator forward to after the value.
+static ExprValue parse_expression_value(TokenIter* iter, StackVariablesArray* variables) {
+    (void)cubs_token_iter_next(iter);
+    const Token token = iter->current;
 
-    ExprValue firstValue = {0};
+    assert(token.tag != SEMICOLON_SYMBOL);
 
-    switch(firstToken) {
+    ExprValue value = {0};
+
+    switch(token.tag) {
         case INT_LITERAL: {
-            firstValue.tag = IntLit;
-            firstValue.value.intLiteral = iter->current.value.intLiteral;
+            value.tag = IntLit;
+            value.value.intLiteral = token.value.intLiteral;
+        } break;
+        case IDENTIFIER: {
+            // TODO handle other kinds of identifiers such as structs function pointers
+            const CubsStringSlice identifier = iter->current.value.identifier;
+            value.tag = Variable;
+            const bool didFind = cubs_stack_variables_array_find(
+                variables, &value.value.variableIndex, identifier);
+            assert(didFind && "Did not find stack variable");     
         } break;
         default: {
-            assert(false && "Cannot handle anything other than int literals");
+            assert(false && "Cannot handle anything other than int literals and variables by identifiers");
         } break;
     }
 
+    (void)cubs_token_iter_next(iter);
+
+    return value;
+}
+
+ExprValue cubs_parse_expression(
+    TokenIter* iter, 
+    StackVariablesArray* variables, 
+    bool hasDestination, 
+    size_t destinationVariableIndex
+) {
+    const ExprValue firstValue = parse_expression_value(iter, variables);
     
-    const TokenType tokenAfterFirst = cubs_token_iter_next(iter);
+    const TokenType tokenAfterFirst = iter->current.tag;
     // Means first token is the only one in the expression
     if(tokenAfterFirst == SEMICOLON_SYMBOL) {
         return firstValue;
