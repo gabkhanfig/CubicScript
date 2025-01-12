e3b46d051f38d5d3c81ab495161dddf9b40772ce
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2025-01-12 18:04:57 -0500

    code gen binary operation add

--- src/compiler/ast_nodes/binary_expression.c
+++ src/compiler/ast_nodes/binary_expression.c
@@ -2,6 +2,8 @@
 #include "../stack_variables.h"
 #include "../../platform/mem.h"
 #include "../../interpreter/function_definition.h"
+#include "../../interpreter/interpreter.h"
+#include "../../interpreter/operations.h"
 
 static void binary_expr_node_deinit(BinaryExprNode* self) {
     expr_value_deinit(&self->lhs);
@@ -14,7 +16,37 @@ static void binary_expr_node_build_function(
     FunctionBuilder* builder,
     const StackVariablesAssignment* stackAssignment
 ) {  
+    assert(self->operation == Add);
 
+    uint16_t lhsSrc;
+    uint16_t rhsSrc;
+
+    // If lhs and rhs are literal values, they need to be loaded
+    if(self->lhs.tag == IntLit) {
+        const union ExprValueMetadata value = self->lhs.value;
+        lhsSrc = (uint16_t)value.intLiteral.variableIndex;
+        Bytecode loadImmediateLong[2];
+        operands_make_load_immediate_long(loadImmediateLong, cubsValueTagInt, lhsSrc, (size_t)value.intLiteral.literal);
+        cubs_function_builder_push_bytecode_many(builder, loadImmediateLong, 2);
+    } else if(self->lhs.tag == Variable) {
+        lhsSrc = self->lhs.value.variableIndex;
+    }
+    else if(self->lhs.tag == Variable) {
+        lhsSrc = self->lhs.value.variableIndex;
+    }
+    if(self->rhs.tag == IntLit) {
+        const union ExprValueMetadata value = self->rhs.value;
+        rhsSrc = (uint16_t)value.intLiteral.variableIndex;
+        Bytecode loadImmediateLong[2];
+        operands_make_load_immediate_long(loadImmediateLong, cubsValueTagInt, rhsSrc, (size_t)value.intLiteral.literal);
+        cubs_function_builder_push_bytecode_many(builder, loadImmediateLong, 2);    
+    }
+    else if(self->rhs.tag == Variable) {
+        rhsSrc = self->rhs.value.variableIndex;
+    }
+
+    const Bytecode addBytecode = operands_make_add_dst(false, self->outputVariableIndex, lhsSrc, rhsSrc);
+    cubs_function_builder_push_bytecode(builder, addBytecode);
 }
 
 static AstNodeVTable binary_expr_node_vtable = {
