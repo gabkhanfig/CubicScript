a5c0e1352be8ae5a3add57854e0e7b05123d9a90
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2025-02-17 15:45:27 -0500

    test multiple functions with and without dependencies

--- src/compiler/graph/function_dependency_graph.zig
+++ src/compiler/graph/function_dependency_graph.zig
@@ -42,7 +42,7 @@ const function_dependency_graph_builder_deinit = c.function_dependency_graph_bui
 const function_dependency_graph_builder_push = c.function_dependency_graph_builder_push;
 const function_dependency_graph_builder_build = c.function_dependency_graph_builder_build;
 
-test "Function with no dependencies" {
+test "one function with no dependencies" {
     const function = function_dependencies_init("testFunc");
 
     var builder = FunctionDependencyGraphBuilder{};
@@ -60,4 +60,78 @@ test "Function with no dependencies" {
         i += 1;
     }
     try expect(i == 1);
+}
+
+test "two functions with no dependencies" {
+    const names = [2][]const u8{ "testFunc1", "testFunc2" };
+
+    const f1 = function_dependencies_init(names[0]);
+    const f2 = function_dependencies_init(names[1]);
+
+    var builder = FunctionDependencyGraphBuilder{};
+    function_dependency_graph_builder_push(&builder, f1);
+    function_dependency_graph_builder_push(&builder, f2);
+
+    var graph = function_dependency_graph_builder_build(&builder);
+    defer function_dependency_graph_deinit(&graph);
+
+    var iter = function_dependency_graph_iter_init(&graph);
+
+    var i: usize = 0;
+    while (function_dependency_graph_iter_next(&iter)) |entry| {
+        try expect(entry.dependenciesLen == 0);
+        try expect(entryNameIs(entry, names[i]));
+        i += 1;
+    }
+    try expect(i == 2);
+}
+
+test "two functions one depend on the other" {
+    const names = [2][]const u8{ "primary", "secondary" };
+
+    const OrderIndependent = struct {
+        fn validate(builder: *FunctionDependencyGraphBuilder) !void {
+            var graph = function_dependency_graph_builder_build(builder);
+            defer function_dependency_graph_deinit(&graph);
+
+            var iter = function_dependency_graph_iter_init(&graph);
+
+            { // first function should have no dependencies
+                const entry = function_dependency_graph_iter_next(&iter).?;
+                try expect(entry.dependenciesLen == 0);
+                try expect(entryNameIs(entry, names[0]));
+            }
+            { // second function should have dependencies
+                const entry = function_dependency_graph_iter_next(&iter).?;
+                try expect(entry.dependenciesLen == 1);
+                try expect(entryNameIs(entry, names[1]));
+                try expect(entryNameIs(entry.dependencies[0], names[0]));
+            }
+
+            try expect(function_dependency_graph_iter_next(&iter) == null);
+        }
+    };
+
+    { // no dependencies pushed first
+        var builder = FunctionDependencyGraphBuilder{};
+        const f1 = function_dependencies_init(names[0]);
+        function_dependency_graph_builder_push(&builder, f1);
+
+        var f2 = function_dependencies_init(names[1]);
+        function_dependencies_push(&f2, names[0]);
+        function_dependency_graph_builder_push(&builder, f2);
+
+        try OrderIndependent.validate(&builder);
+    }
+    { // no dependencies pushed after
+        var builder = FunctionDependencyGraphBuilder{};
+        var f1 = function_dependencies_init(names[1]);
+        function_dependencies_push(&f1, names[0]);
+        function_dependency_graph_builder_push(&builder, f1);
+
+        const f2 = function_dependencies_init(names[0]);
+        function_dependency_graph_builder_push(&builder, f2);
+
+        try OrderIndependent.validate(&builder);
+    }
 }
