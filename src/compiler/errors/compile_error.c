39ff8e503e13b8099d1c3a0c909ee166ae49ed30
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2025-04-20 17:32:14 -0400

    change compile error to include consistent data

--- src/compiler/errors/compile_error.c
+++ src/compiler/errors/compile_error.c
@@ -3,18 +3,7 @@
 
 void cubs_compile_error_deinit(CompileError *self)
 {
+    cubs_string_deinit(&self->message);
     assert(self->vtable->deinit != NULL);
     self->vtable->deinit(self->ptr);
-}
-
-CubsCompileErrorLocation cubs_compile_error_where(const CompileError *self)
-{
-    assert(self->vtable->where != NULL);
-    return self->vtable->where(self->ptr);
-}
-
-CubsStringSlice cubs_compile_error_what(const CompileError *self)
-{
-    assert(self->vtable->what != NULL);
-    return self->vtable->what(self->ptr);
 }
--- src/compiler/errors/compile_error.h
+++ src/compiler/errors/compile_error.h
@@ -3,29 +3,55 @@
 #define COMPILE_ERROR_H
 
 #include "../../primitives/string/string_slice.h"
+#include "../../primitives/string/string.h"
 #include "../compiler.h"
 
+typedef enum CompileErrorType {
+    compileErrorTypeUnexpectedToken,
+} CompileErrorType;
+
 typedef void(*CompileErrorDeinit)(void* self);
-/// Gets the source file location of the error.
-typedef CubsCompileErrorLocation (*CompileErrorWhere)(const void* self);
-/// Gets the error message of the compile error.
-typedef CubsStringSlice (*CompileErrorWhat)(const void* self);
 
 typedef struct CompileErrorVTable {
-    CompileErrorDeinit deinit;
-    CompileErrorWhere where;
-    CompileErrorWhat what;
+    CompileErrorType    errType;
+    CompileErrorDeinit  deinit;
 } CompileErrorVTable;
 
 typedef struct CompileError {
-    void* ptr;
-    const CompileErrorVTable* vtable;
+    const CompileErrorVTable*   vtable;
+    void*                       ptr;
+    CubsCompileErrorLocation    location;
+    CubsString                  message;
 } CompileError;
 
 void cubs_compile_error_deinit(CompileError* self);
 
-CubsCompileErrorLocation cubs_compile_error_where(const CompileError* self);
+static inline CubsString charPosToString(CubsSourceFileCharPosition pos) {
+    CubsString temp = {0};
+    CubsString out = cubs_string_init_unchecked((CubsStringSlice){.str = "Byte ", .len = 5});
 
-CubsStringSlice cubs_compile_error_what(const CompileError* self);
+    CubsString byteString = cubs_string_from_int(pos.index);
+    temp = cubs_string_concat(&out, &byteString);
+    cubs_string_deinit(&out);
+    cubs_string_deinit(&byteString);
+
+    out = cubs_string_concat_slice_unchecked(&temp, (CubsStringSlice){.str = ", Ln ", .len = 5});
+    cubs_string_deinit(&temp);
+    
+    CubsString lineString = cubs_string_from_int(pos.line);
+    temp = cubs_string_concat(&out, &cubs_string_concat);
+    cubs_string_deinit(&out);
+    cubs_string_deinit(&lineString);
+
+    out = cubs_string_concat_slice_unchecked(&temp, (CubsStringSlice){.str = ", Col ", .len = 6});
+    cubs_string_deinit(&temp);
+
+    CubsString columnString = cubs_string_from_int(pos.column);
+    temp = cubs_string_concat(&out, &cubs_string_concat);
+    cubs_string_deinit(&out);
+    cubs_string_deinit(&columnString);
+
+    return temp;
+}
 
 #endif
